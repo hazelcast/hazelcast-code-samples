617933373ecee97ba7afdd56a457bcd3416c658e
alarmnummer <alarmnummer@gmail.com>
2013-08-03 16:33:25 +0300

    Added byte-array-serializer example

---
+++ chapter-serialization/byte-array-serializer/src/main/java/Person.java
@@ -0,0 +1,21 @@
+public class Person {
+    private String name;
+
+    public Person() {
+    }
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
---
+++ chapter-serialization/byte-array-serializer/src/main/java/PersonByteArraySerializer.java
@@ -0,0 +1,27 @@
+import com.hazelcast.nio.serialization.ByteArraySerializer;
+
+import java.io.IOException;
+
+public class PersonByteArraySerializer implements ByteArraySerializer<Person> {
+
+    @Override
+    public void destroy() {
+    }
+
+    @Override
+    public int getTypeId() {
+        return 1;
+    }
+
+    @Override
+    public byte[] write(Person object) throws IOException {
+        System.out.println("Writing");
+        return object.getName().getBytes();
+    }
+
+    @Override
+    public Person read(byte[] buffer) throws IOException {
+        String name = new String(buffer);
+        return new Person(name);
+    }
+}
