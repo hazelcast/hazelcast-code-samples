0945cbf9cdbc01e6126fa0f526be31816f3a3a96
Peter Veentjer <alarmnummer@gmail.com>
2013-08-28 17:05:43 +0300

    Lots of work on entry processor

---
+++ chapter-distributed-map/entry-processor/src/main/java/Employee.java
@@ -0,0 +1,17 @@
+import java.io.Serializable;
+
+public class Employee implements Serializable {
+    private int salary;
+
+    public Employee(int salary) {
+        this.salary = salary;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void incSalary(int delta){
+        salary+=delta;
+    }
+}
