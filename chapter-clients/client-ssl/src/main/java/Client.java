a3378b445e43b81c34e9c1c83d6d75c89e35639e
alarmnummer <alarmnummer@gmail.com>
2013-09-01 16:59:46 +0300

    Lots of work on the client

---
+++ chapter-clients/client-ssl/src/main/java/Client.java
@@ -0,0 +1,27 @@
+import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.client.connection.SSLSocketFactory;
+import com.hazelcast.core.HazelcastInstance;
+
+import java.io.File;
+import java.util.concurrent.BlockingQueue;
+
+public class Client {
+    public static void main(String[] args) throws Exception {
+        System.setProperty("javax.net.ssl.keyStore", new File("hazelcast.ks").getAbsolutePath());
+        System.setProperty("javax.net.ssl.trustStore", new File("hazelcast.ts").getAbsolutePath());
+        System.setProperty("javax.net.ssl.keyStorePassword", "password");
+
+
+        ClientConfig config = new ClientConfig();
+        config.addAddress("127.0.0.1");
+        config.setRedoOperation(true);
+        config.getSocketOptions().setSocketFactory(new SSLSocketFactory());
+
+        HazelcastInstance client = HazelcastClient.newHazelcastClient(config);
+        BlockingQueue <String> queue = client.getQueue("queue");
+        queue.put("Hello!");
+        System.out.println("Message send by client!");
+        System.exit(0);
+    }
+}
---
+++ chapter-clients/client-ssl/src/main/java/FullMember.java
@@ -0,0 +1,30 @@
+import com.hazelcast.config.Config;
+import com.hazelcast.config.SSLConfig;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+
+import java.io.File;
+import java.util.concurrent.BlockingQueue;
+
+
+/**
+ * keytool -genkey -alias hazelcast -keyalg RSA -keypass password -keystore hazelcast.ks -storepass password
+ * keytool -export -alias hazelcast -file hazelcast.cer -keystore hazelcast.ks -storepass password
+ * keytool -import -v -trustcacerts -alias hazelcast -keypass password -file hazelcast.cer -keystore hazelcast.ts -storepass password
+ */
+public class FullMember {
+    public static void main(String[] args) throws Exception {
+        System.setProperty("javax.net.ssl.keyStore", new File("hazelcast.ks").getAbsolutePath());
+        System.setProperty("javax.net.ssl.trustStore", new File("hazelcast.ts").getAbsolutePath());
+        System.setProperty("javax.net.ssl.keyStorePassword", "password");
+
+        Config config = new Config();
+        config.getNetworkConfig().setSSLConfig(new SSLConfig().setEnabled(true));
+
+        HazelcastInstance hz = Hazelcast.newHazelcastInstance(config);
+        BlockingQueue<String> queue = hz.getQueue("queue");
+        System.out.println("Full member up");
+        for (; ; )
+            System.out.println(queue.take());
+    }
+}
