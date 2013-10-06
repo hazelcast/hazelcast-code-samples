bb106947da798942be41a27d85f2d4883c99c3a6
Peter Veentjer <alarmnummer@gmail.com>
2013-10-06 07:31:17 +0300

    Work on serialization (added kryo example)

---
+++ chapter-serialization/kryo-serializer/src/main/java/Person.java
@@ -0,0 +1,14 @@
+public class Person{
+    private String name;
+
+    private Person(){}
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Person(name=%s)", name);
+    }
+}
