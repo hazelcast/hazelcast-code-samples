96d12f0cbb7ddddab60a9f8acb037ac33aa6b358
Peter Veentjer <alarmnummer@gmail.com>
2013-10-18 15:08:43 +0200

    Minor improvements

--- chapter-spi/proxy/src/main/java/CounterProxy.java
+++ chapter-spi/proxy/src/main/java/CounterProxy.java
@@ -3,7 +3,6 @@ import com.hazelcast.spi.InvocationBuilder;
 import com.hazelcast.spi.NodeEngine;
 import com.hazelcast.util.ExceptionUtil;
 
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
 public class CounterProxy implements Counter {
