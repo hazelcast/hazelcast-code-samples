1f28389adf753fd4b7e62218f7fe98434e507c6d
alarmnummer <alarmnummer@gmail.com>
2013-12-01 10:56:14 +0200

    Work on atomiclong

--- chapter-distributed-primitives/atomiclong-functions/src/main/java/Member.java
+++ chapter-distributed-primitives/atomiclong-functions/src/main/java/Member.java
@@ -1,3 +1,4 @@
+import com.hazelcast.core.Function;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.core.IAtomicLong;
@@ -9,12 +10,40 @@ public class Member {
         HazelcastInstance hz = Hazelcast.newHazelcastInstance();
         IAtomicLong atomicLong = hz.getAtomicLong("counter");
 
+        atomicLong.set(1);
+        long result = atomicLong.apply(new Add2Function());
+        System.out.println("apply.result:" + result);
+        System.out.println("apply.value:" + atomicLong.get());
 
-        System.out.printf("Count is %s\n", atomicLong.get());
+        atomicLong.set(1);
+        atomicLong.alter(new Add2Function());
+        System.out.println("alter.value:"+atomicLong.get());
+
+        atomicLong.set(1);
+        result = atomicLong.alterAndGet(new Add2Function());
+        System.out.println("alterAndGet.result:" + result);
+        System.out.println("alterAndGet.value:"+atomicLong.get());
+
+        atomicLong.set(1);
+        result = atomicLong.getAndAlter(new Add2Function());
+        System.out.println("getAndAlter.result:"+result);
+        System.out.println("getAndAlter.value:"+atomicLong.get());
+
         System.exit(0);
+
+        for(;;){
+            long oldValue = atomicLong.get();
+            long newValue = oldValue+2;
+            if(atomicLong.compareAndSet(oldValue,newValue)){
+                break;
+            }
+        }
     }
 
-    private static class DoubleFunction implements Function<Long,Long>{
-
+    private static class Add2Function implements Function<Long,Long> {
+        @Override
+        public Long apply(Long input) {
+            return input+2;
+        }
     }
 }
