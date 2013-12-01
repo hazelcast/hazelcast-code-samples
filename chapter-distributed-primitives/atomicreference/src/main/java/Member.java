cfe94b29292de783fe138e475d8fa97cf0bd4875
alarmnummer <alarmnummer@gmail.com>
2013-12-01 20:18:57 +0200

    AtomicReference stuff

--- chapter-distributed-primitives/atomicreference/src/main/java/Member.java
+++ chapter-distributed-primitives/atomicreference/src/main/java/Member.java
@@ -1,9 +1,31 @@
+import com.hazelcast.config.Config;
+import com.hazelcast.config.InMemoryFormat;
+import com.hazelcast.config.MapConfig;
 import com.hazelcast.core.*;
 
 public class Member {
     public static void main(String[] args) {
-        HazelcastInstance hz = Hazelcast.newHazelcastInstance();
-        IAtomicReference<Double> ref = hz.getAtomicReference("reference");
+        MapConfig mapConfig = new MapConfig("foo");
+        mapConfig.setInMemoryFormat(InMemoryFormat.OBJECT);
+
+        Config config=new Config();
+        config.addMapConfig(mapConfig);
+
+
+        HazelcastInstance hz = Hazelcast.newHazelcastInstance(config);
+
+        String value = "bla";
+        IMap map = hz.getMap(mapConfig.getName());
+        map.put("1",value);
+
+        System.out.println("value == map.get:"+(value==map.get("1")));
+        System.out.println("map.get == map.get:"+(map.get("1")==map.get("1")));
+        if(true){
+           return;
+       }
+
+
+        IAtomicReference <Double> ref = hz.getAtomicReference("reference");
         ref.compareAndSet(null,new Double(0));
         for (int k = 0; k < 1000 * 1000; k++) {
             if (k % 500000 == 0) {
