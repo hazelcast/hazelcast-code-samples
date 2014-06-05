ecdd24cb64195bd37835344662172000ef9ad670
noctarius <me@noctarius.com>
2014-06-05 22:22:13 +0200

    Added comments for aggregations examples

--- chapter-distributed-map/aggregations/src/main/java/AggregationsDemo.java
+++ chapter-distributed-map/aggregations/src/main/java/AggregationsDemo.java
@@ -3,6 +3,7 @@ import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.core.IMap;
 import com.hazelcast.mapreduce.aggregation.Aggregation;
 import com.hazelcast.mapreduce.aggregation.Aggregations;
+import com.hazelcast.mapreduce.aggregation.PropertyExtractor;
 import com.hazelcast.mapreduce.aggregation.Supplier;
 import com.hazelcast.query.Predicate;
 
@@ -30,14 +31,19 @@ public class AggregationsDemo {
             // Fill in demo data
             fillEmployeeMap(employees);
 
+            // We simple calculate a average over all salaries on all employees
             simpleSalaryAverage(employees);
 
+            // Here we first search Hazelcast employees using the Predicate and calculate the average only on those
             companyBasedSalaryAverage(employees);
 
+            // Now we just sum up the salaries
             salarySum(employees);
 
+            // This time we count the employees that we calculate on (let's expect it to be 10k ;-))
             countEmployees(employees);
 
+            // Last but not least we want to get all distinct first names of the employees
             distinctEmployeeFirstNames(employees);
 
         } finally {
@@ -47,8 +53,16 @@ public class AggregationsDemo {
     }
 
     private static void simpleSalaryAverage(IMap<String, Employee> employees) {
-        Supplier<String, Employee, Integer> supplier = Supplier.all(new SalaryPropertyExtractor());
+        // Create the PropertyExtractor to extract salary value from the employee
+        PropertyExtractor<Employee, Integer> propertyExtractor = new SalaryPropertyExtractor();
+
+        // Select all employees
+        Supplier<String, Employee, Integer> supplier = Supplier.all(propertyExtractor);
+
+        // Choose the aggregation to perform
         Aggregation<String, Integer, Integer> aggregation = Aggregations.integerAvg();
+
+        // Execute the aggregation and print the result
         int avgSalary = employees.aggregate(supplier, aggregation);
         System.out.println("Overall average salary: " + avgSalary);
 
@@ -57,10 +71,22 @@ public class AggregationsDemo {
     }
 
     private static void companyBasedSalaryAverage(IMap<String, Employee> employees) {
+        // Create the Predicate to select only Hazelcast employees
         Predicate<String, Employee> companyPredicate = new CompanyPredicate("Hazelcast");
-        Supplier<String, Employee, Integer> salaryExtractor = Supplier.all(new SalaryPropertyExtractor());
+
+        // Create the PropertyExtractor to extract salary value from the employee
+        PropertyExtractor<Employee, Integer> propertyExtractor = new SalaryPropertyExtractor();
+
+        // Create the supplier to handle extracted salaries from selected employees
+        Supplier<String, Employee, Integer> salaryExtractor = Supplier.all(propertyExtractor);
+
+        // Merge Predicate and PropertyExtractor (through the previous Supplier) together
         Supplier<String, Employee, Integer> supplier = Supplier.fromPredicate(companyPredicate, salaryExtractor);
+
+        // Choose the aggregation to perform
         Aggregation<String, Integer, Integer> aggregation = Aggregations.integerAvg();
+
+        // Execute the aggregation and print the result
         int avgSalary = employees.aggregate(supplier, aggregation);
         System.out.println("Hazelcast average salary: " + avgSalary);
 
@@ -73,8 +99,16 @@ public class AggregationsDemo {
     }
 
     private static void salarySum(IMap<String, Employee> employees) {
-        Supplier<String, Employee, Integer> supplier = Supplier.all(new SalaryPropertyExtractor());
+        // Create the PropertyExtractor to extract salary value from the employee
+        PropertyExtractor<Employee, Integer> propertyExtractor = new SalaryPropertyExtractor();
+
+        // Select all employees
+        Supplier<String, Employee, Integer> supplier = Supplier.all(propertyExtractor);
+
+        // Choose the aggregation to perform
         Aggregation<String, Integer, Integer> aggregation = Aggregations.integerSum();
+
+        // Execute the aggregation and print the result
         int sumSalary = employees.aggregate(supplier, aggregation);
         System.out.println("Sum of all salaries: " + sumSalary);
 
@@ -83,8 +117,13 @@ public class AggregationsDemo {
     }
 
     private static void countEmployees(IMap<String, Employee> employees) {
+        // Select all employees
         Supplier<String, Employee, Object> supplier = Supplier.all();
+
+        // Choose the aggregation to perform
         Aggregation<String, Object, Long> aggregation = Aggregations.count();
+
+        // Execute the aggregation and print the result
         long countEmployee = employees.aggregate(supplier, aggregation);
         System.out.println("Number of employees: " + countEmployee);
 
@@ -93,8 +132,16 @@ public class AggregationsDemo {
     }
 
     private static void distinctEmployeeFirstNames(IMap<String, Employee> employees) {
-        Supplier<String, Employee, String> supplier = Supplier.all(new FirstNamePropertyExtractor());
+        // Create PropertyExtractor to extract firstName value from the employee
+        PropertyExtractor<Employee, String> propertyExtractor = new FirstNamePropertyExtractor();
+
+        // Select all employees
+        Supplier<String, Employee, String> supplier = Supplier.all(propertyExtractor);
+
+        // Choose the aggregation to perform
         Aggregation<String, String, Set<String>> aggregation = Aggregations.distinctValues();
+
+        // Execute the aggregation and print the result
         Set<String> allFirstNames = employees.aggregate(supplier, aggregation);
         System.out.println("All first names: " + allFirstNames);
 
