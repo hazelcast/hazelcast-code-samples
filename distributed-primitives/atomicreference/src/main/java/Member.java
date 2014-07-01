3941a426a69338469b4186814bce375660d499c8
Peter Veentjer <alarmnummer@gmail.com>
2014-07-01 13:08:20 +0300

    IAtomicReference cleanup

--- distributed-primitives/atomicreference/src/main/java/Member.java
+++ distributed-primitives/atomicreference/src/main/java/Member.java
@@ -1,30 +1,14 @@
-import com.hazelcast.config.Config;
-import com.hazelcast.config.InMemoryFormat;
-import com.hazelcast.config.MapConfig;
-import com.hazelcast.core.*;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IAtomicReference;
 
 public class Member {
     public static void main(String[] args) {
-        Config config = new Config();
+        HazelcastInstance hz = Hazelcast.newHazelcastInstance();
 
-        HazelcastInstance hz = Hazelcast.newHazelcastInstance(config);
-
-        IAtomicReference<Double> ref = hz.getAtomicReference("reference");
-        ref.compareAndSet(null, new Double(0));
-        for (int k = 0; k < 1000 * 1000; k++) {
-            if (k % 500000 == 0) {
-                System.out.println("At: " + k);
-            }
-            ref.alter(new IncFuntion());
-        }
-        System.out.printf("Ref is %s\n", ref.get());
+        IAtomicReference<String> ref = hz.getAtomicReference("reference");
+        ref.set("foo");
+        System.out.println(ref.get());
         System.exit(0);
-    }
-
-    public static class IncFuntion implements IFunction<Double, Double> {
-        @Override
-        public Double apply(Double input) {
-            return input++;
-        }
     }
 }
