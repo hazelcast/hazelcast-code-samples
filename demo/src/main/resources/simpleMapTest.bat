7c93a433a54f11951e71b6baaa475e1a2c21b7c7
noctarius <me@noctarius.com>
2014-07-23 14:09:15 +0200

    Fixing batch script

--- demo/src/main/resources/simpleMapTest.bat
+++ demo/src/main/resources/simpleMapTest.bat
@@ -1,1 +1,1 @@
-java -server -cp -cp ../target/demo-${project.version}.jar;../target/lib/hazelcast-all-${hazelcast.version}.jar com.hazelcast.demo.SimpleMapTest %1 %2 %3 %4 %5 %6
+java -server -cp ../target/demo-${project.version}.jar;../target/lib/hazelcast-all-${hazelcast.version}.jar com.hazelcast.demo.SimpleMapTest %1 %2 %3 %4 %5 %6
