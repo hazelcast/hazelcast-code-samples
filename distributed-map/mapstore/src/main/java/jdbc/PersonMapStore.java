e000d87a76f4f66da18d1585fa1d78ff9583685a
ajermakovics <andrejs.jermakovics@gmail.com>
2015-01-12 13:05:33 +0000

    Simplified example a bit. Moved prepared statement to mapstore.

--- distributed-map/mapstore/src/main/java/jdbc/PersonMapStore.java
+++ distributed-map/mapstore/src/main/java/jdbc/PersonMapStore.java
@@ -14,13 +14,16 @@ import static java.lang.String.format;
 import data.Person;
 
 public class PersonMapStore implements MapStore<Long, Person> {
+
     private final Connection con;
+    private PreparedStatement allKeysStatement;
 
     public PersonMapStore() {
         try {
             con = DriverManager.getConnection("jdbc:hsqldb:mydatabase", "SA", "");
             con.createStatement().executeUpdate(
                     "create table if not exists person (id bigint not null, name varchar(45), primary key (id))");
+            allKeysStatement = con.prepareStatement("select id from person");
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -77,12 +80,7 @@ public class PersonMapStore implements MapStore<Long, Person> {
     }
 
     public Iterable<Long> loadAllKeys() {
-        try {
-            PreparedStatement statement = con.prepareStatement("select id from person");
-            return new StatementIterable<Long>(statement);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        return new StatementIterable<Long>(allKeysStatement);
     }
 
 }
