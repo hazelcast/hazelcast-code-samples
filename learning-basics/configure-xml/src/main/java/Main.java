4e84ed4d0dfdedfcccb4f59b87951b872c3d01e5
bilalyasar <bilalyasar45@gmail.com>
2015-02-24 17:52:33 +0200

    client config import added

--- learning-basics/configure-xml/src/main/java/Main.java
+++ learning-basics/configure-xml/src/main/java/Main.java
@@ -1,8 +1,10 @@
+import com.hazelcast.client.HazelcastClient;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
 
 public class Main {
     public static void main(String[] args) {
         HazelcastInstance hz = Hazelcast.newHazelcastInstance();
+        HazelcastInstance client = HazelcastClient.newHazelcastClient();
     }
 }
