da19803957f93eda340169f8746a8b57997ce3b0
Viktor Gamov <viktor@hazelcast.com>
2015-05-12 16:19:09 -0400

    Fixes in HD memory examples after code review
    Squashed commit of the following:
    commit 7edf14b066d2d3f7cfdeb320749b48ca3df7235c
    Author: Viktor Gamov <viktor@hazelcast.com>
    Date:   Tue May 12 16:18:00 2015 -0400
    
        Using EnterpriseSerializationService to access Native memory stats
    
    commit 224fdcd8052ed9d4a51e612a4311b88c8a54ae97
    Author: Viktor Gamov <viktor@hazelcast.com>
    Date:   Tue May 12 15:38:16 2015 -0400
    
        removed unused Hd near cache config
    
    commit 2651fbfdfb446d8f5146a8fb215a5157fec90b26
    Merge: 8eabcc6 d891b96
    Author: Viktor Gamov <viktor@hazelcast.com>
    Date:   Tue May 12 15:37:22 2015 -0400
    
        Merge branch 'master' into hd-dbrimley
    
    commit 8eabcc6d8ee43fc372f201bb4bfb61532bf3f6f1
    Author: Viktor Gamov <gamussa@gmail.com>
    Date:   Tue May 12 12:25:51 2015 -0400
    
        added trial key link
    
    commit afcecc160fa714fd841076d703cc88ea4c7bf612
    Author: Viktor Gamov <gamussa@gmail.com>
    Date:   Tue May 12 12:22:57 2015 -0400
    
        refactored hazelcast config xml for client and server
    
    commit 00027ac4f8e7fc0af9a520375431dce74e3c2578
    Author: Viktor Gamov <gamussa@gmail.com>
    Date:   Tue May 12 12:22:21 2015 -0400
    
        pom.xml: added hd-memory client/server example to enterprise examples
    
    commit c6cb38e06b1ff60520b9a8e1f1f2e63863dda9dd
    Author: Viktor Gamov <gamussa@gmail.com>
    Date:   Tue May 12 12:18:54 2015 -0400
    
        package refactoring
    
    commit 8e7413ceb8b25512b85bc58456a0801960bc1102
    Author: Viktor Gamov <gamussa@gmail.com>
    Date:   Tue May 12 12:11:28 2015 -0400
    
        removed leftovers
    
    commit 3b20aa252e913869293a32e6d1339956b7f7ec17
    Merge: 8fb9c2b 86c0cbe
    Author: Viktor Gamov <gamussa@gmail.com>
    Date:   Tue May 12 10:50:42 2015 -0400
    
        Merge branch 'master' into hd-dbrimley
    
    commit 8fb9c2bc3f3a77157c73c05fa79e4009945277b0
    Merge: cdbbd39 df9a450
    Author: Viktor Gamov <gamussa@gmail.com>
    Date:   Tue May 12 10:03:01 2015 -0400
    
        Merge commit 'df9a450370633df1ef988071791096ca6932ef88' as 'enterprise/hd-memory-client-server'
    
    commit df9a450370633df1ef988071791096ca6932ef88
    Author: Viktor Gamov <gamussa@gmail.com>
    Date:   Tue May 12 10:03:01 2015 -0400
    
        Squashed 'enterprise/hd-memory-client-server/' content from commit eea0bb8
    
        git-subtree-dir: enterprise/hd-memory-client-server
        git-subtree-split: eea0bb8dc99c43339c159b847ec9a9b74d11e9b4

--- enterprise/hd-memory-client-server/hd-memory-examples-server/src/main/java/MemoryStatsUtil.java
+++ enterprise/hd-memory-client-server/hd-memory-examples-server/src/main/java/MemoryStatsUtil.java
@@ -1,8 +1,11 @@
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.instance.HazelcastInstanceImpl;
 import com.hazelcast.instance.HazelcastInstanceProxy;
+import com.hazelcast.instance.Node;
 import com.hazelcast.memory.DefaultMemoryStats;
+import com.hazelcast.memory.MemoryManager;
 import com.hazelcast.memory.MemoryStats;
+import com.hazelcast.nio.serialization.EnterpriseSerializationService;
 import com.hazelcast.util.ExceptionUtil;
 
 import java.lang.reflect.Method;
@@ -10,17 +13,35 @@ import java.lang.reflect.Method;
 public class MemoryStatsUtil {
 
     public static MemoryStats getMemoryStats(HazelcastInstance hz) {
-        try {
-            if (hz instanceof HazelcastInstanceProxy) {
-                Method original = HazelcastInstanceProxy.class.getDeclaredMethod("getOriginal");
+        Node node = getNode(hz); // or another way for getting "Node" over "HazelcastInstance"
+        if (node != null) {
+            EnterpriseSerializationService serializationService =
+                (EnterpriseSerializationService) node.getSerializationService();
+            MemoryManager memoryManager = serializationService.getMemoryManager();
+            return memoryManager.getMemoryStats();
+        } else
+            return new DefaultMemoryStats();
+    }
+
+    public static Node getNode(HazelcastInstance hz) {
+        HazelcastInstanceImpl impl = getHazelcastInstanceImpl(hz);
+        return impl != null ? impl.node : null;
+    }
+
+    public static HazelcastInstanceImpl getHazelcastInstanceImpl(HazelcastInstance hz) {
+        HazelcastInstanceImpl impl = null;
+        if (hz instanceof HazelcastInstanceProxy) {
+            Method original = null;
+            try {
+                original = HazelcastInstanceProxy.class.getDeclaredMethod("getOriginal");
                 original.setAccessible(true);
-                return ((HazelcastInstanceImpl) original.invoke(hz)).getMemoryStats();
-            } else if (hz instanceof HazelcastInstanceImpl) {
-                return ((HazelcastInstanceImpl) hz).getMemoryStats();
+                impl = ((HazelcastInstanceImpl) original.invoke(hz));
+            } catch (Exception e) {
+                return ExceptionUtil.sneakyThrow(e);
             }
-        } catch (Exception e) {
-            return ExceptionUtil.sneakyThrow(e);
+        } else if (hz instanceof HazelcastInstanceImpl) {
+            impl = (HazelcastInstanceImpl) hz;
         }
-        return new DefaultMemoryStats();
+        return impl;
     }
 }
