e452a1063092bdd75cccfe86de3b904fc1c53877
Viktor Gamov <gamussa@gmail.com>
2015-05-12 12:28:20 -0400

    HD Memory client-server example
    Squashed commit of the following:
    commit 8eabcc6d8ee43fc372f201bb4bfb61532bf3f6f1
    Author: Viktor Gamov <gamussa@gmail.com>
    Date:   Tue May 12 12:25:51 2015 -0400
    
        added trial key link
    
    commit afcecc160fa714fd841076d703cc88ea4c7bf612
    Author: Viktor Gamov <gamussa@gmail.com>
    Date:   Tue May 12 12:22:57 2015 -0400
    
        refactored hazelcast config xml for client and server
    
    commit 00027ac4f8e7fc0af9a520375431dce74e3c2578
    Author: Viktor Gamov <gamussa@gmail.com>
    Date:   Tue May 12 12:22:21 2015 -0400
    
        pom.xml: added hd-memory client/server example to enterprise examples
    
    commit c6cb38e06b1ff60520b9a8e1f1f2e63863dda9dd
    Author: Viktor Gamov <gamussa@gmail.com>
    Date:   Tue May 12 12:18:54 2015 -0400
    
        package refactoring
    
    commit 8e7413ceb8b25512b85bc58456a0801960bc1102
    Author: Viktor Gamov <gamussa@gmail.com>
    Date:   Tue May 12 12:11:28 2015 -0400
    
        removed leftovers
    
    commit 3b20aa252e913869293a32e6d1339956b7f7ec17
    Merge: 8fb9c2b 86c0cbe
    Author: Viktor Gamov <gamussa@gmail.com>
    Date:   Tue May 12 10:50:42 2015 -0400
    
        Merge branch 'master' into hd-dbrimley
    
    commit 8fb9c2bc3f3a77157c73c05fa79e4009945277b0
    Merge: cdbbd39 df9a450
    Author: Viktor Gamov <gamussa@gmail.com>
    Date:   Tue May 12 10:03:01 2015 -0400
    
        Merge commit 'df9a450370633df1ef988071791096ca6932ef88' as 'enterprise/hd-memory-client-server'
    
    commit df9a450370633df1ef988071791096ca6932ef88
    Author: Viktor Gamov <gamussa@gmail.com>
    Date:   Tue May 12 10:03:01 2015 -0400
    
        Squashed 'enterprise/hd-memory-client-server/' content from commit eea0bb8
    
        git-subtree-dir: enterprise/hd-memory-client-server
        git-subtree-split: eea0bb8dc99c43339c159b847ec9a9b74d11e9b4

---
+++ enterprise/hd-memory-client-server/hd-memory-examples-client/src/main/java/EnterpriseCacheTestClient.java
@@ -0,0 +1,275 @@
+import com.hazelcast.cache.ICache;
+import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.client.cache.impl.HazelcastClientCachingProvider;
+import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.client.config.XmlClientConfigBuilder;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.logging.ILogger;
+import com.hazelcast.logging.Logger;
+import com.hazelcast.memory.NativeOutOfMemoryError;
+import org.HdrHistogram.AbstractHistogram;
+import org.HdrHistogram.AtomicHistogram;
+import org.HdrHistogram.Histogram;
+
+import javax.cache.CacheManager;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class EnterpriseCacheTestClient {
+
+    private static final String NAMESPACE = "test";
+    private static final long STATS_SECONDS = 10;
+
+    private final HazelcastInstance instance;
+    private final CacheManager cacheManager;
+    private final ILogger logger;
+    private final Stats[] allStats;
+
+    private final ExecutorService ex;
+    private final int threadCount;
+    private final int getPercentage;
+    private final int putPercentage;
+    private final int keyRange;
+    private final long duration;
+    private final String[] values = new String[500];
+    private final AtomicBoolean live = new AtomicBoolean(true);
+
+    static {
+        System.setProperty("hazelcast.version.check.enabled", "false");
+        System.setProperty("hazelcast.socket.bind.any", "false");
+        System.setProperty("java.net.preferIPv4Stack", "true");
+        System.setProperty("hazelcast.multicast.group", "224.35.57.79");
+    }
+
+    public EnterpriseCacheTestClient(int threadCount, int getPercentage, int putPercentage,
+        int keyRange, int valueMin,
+        int valueMax, long duration) throws IOException {
+        this.threadCount = threadCount;
+        this.getPercentage = getPercentage;
+        this.putPercentage = putPercentage;
+        this.keyRange = keyRange;
+        this.duration = duration;
+
+        this.allStats = new Stats[threadCount];
+        for (int i = 0; i < threadCount; i++) {
+            allStats[i] = new Stats();
+        }
+        logger = Logger.getLogger(getClass());
+
+        InputStream configInputStream =
+            EnterpriseCacheTestClient.class.getResourceAsStream("/hazelcast-client-hd-memory.xml");
+        ClientConfig clientConfig = new XmlClientConfigBuilder(configInputStream).build();
+
+        buildRandomValues(valueMin, valueMax);
+
+        instance = HazelcastClient.newHazelcastClient(clientConfig);
+        cacheManager =
+            HazelcastClientCachingProvider.createCachingProvider(instance).getCacheManager();
+
+        ex = Executors.newFixedThreadPool(threadCount);
+    }
+
+    private void buildRandomValues(int valueMin, int valueMax) {
+        Random rand = new Random();
+        for (int i = 0; i < values.length; i++) {
+            int size = rand.nextInt(valueMax - valueMin) + valueMin;
+            byte[] bb = new byte[size];
+            rand.nextBytes(bb);
+            values[i] = new String(bb);
+        }
+    }
+
+    public static void main(String[] input) throws Exception {
+        int threadCount = 40;
+        int valueMin = 100;
+        int valueMax = 101;
+        int getPercentage = 40;
+        int putPercentage = 40;
+        int keyRange = Integer.MAX_VALUE;
+        long duration = Long.MAX_VALUE;
+
+        if (input != null && input.length > 0) {
+            for (String arg : input) {
+                arg = arg.trim();
+                if (arg.startsWith("vs")) {
+                    valueMin = Integer.parseInt(arg.substring(2));
+                } else if (arg.startsWith("vx")) {
+                    valueMax = Integer.parseInt(arg.substring(2));
+                } else if (arg.startsWith("t")) {
+                    threadCount = Integer.parseInt(arg.substring(1));
+                } else if (arg.startsWith("g")) {
+                    getPercentage = Integer.parseInt(arg.substring(1));
+                } else if (arg.startsWith("p")) {
+                    putPercentage = Integer.parseInt(arg.substring(1));
+                } else if (arg.startsWith("k")) {
+                    keyRange = Integer.parseInt(arg.substring(1));
+                    if (keyRange <= 0) {
+                        keyRange = Integer.MAX_VALUE;
+                    }
+                } else if (arg.startsWith("d")) {
+                    duration = TimeUnit.MINUTES.toMillis(Integer.parseInt(arg.substring(1)));
+                    if (duration <= 0) {
+                        duration = Long.MAX_VALUE;
+                    }
+                }
+            }
+        }
+
+        EnterpriseCacheTestClient test =
+            new EnterpriseCacheTestClient(threadCount, getPercentage, putPercentage,
+                keyRange, valueMin, valueMax, duration);
+        test.start();
+    }
+
+    private void start() throws InterruptedException {
+        printVariables();
+        startPrintStats();
+        run();
+    }
+
+    private void run() {
+        final ICache<Object, Object> cache = cacheManager.getCache(NAMESPACE).unwrap(ICache.class);
+        for (int i = 0; i < threadCount; i++) {
+            final int tid = i;
+            ex.execute(new Runnable() {
+
+                public void run() {
+                    Random rand = new Random();
+                    String[] vv = values;
+                    Stats stats = allStats[tid];
+                    int stopCounter = 0;
+                    while (true) {
+                        if (stopCounter++ == 5000) {
+                            stopCounter = 0;
+                            if (!live.get()) {
+                                return;
+                            }
+                        }
+                        try {
+                            Object key = newKey(rand);
+                            int operation = rand.nextInt(100);
+                            long start = System.nanoTime();
+                            if (operation < getPercentage) {
+                                cache.get(key);
+                                stats.gets.incrementAndGet();
+                            } else if (operation < getPercentage + putPercentage) {
+                                try {
+                                    cache.put(key, vv[rand.nextInt(vv.length)]);
+                                } catch (NativeOutOfMemoryError e) {
+                                    System.err.println(e.getMessage());
+                                }
+                                stats.puts.incrementAndGet();
+                            } else {
+                                cache.remove(key);
+                                stats.removes.incrementAndGet();
+                            }
+                            long end = System.nanoTime();
+
+                            try {
+                                stats.histogram.recordValue(end - start);
+                            } catch (IndexOutOfBoundsException ignored) {
+                            }
+                        } catch (Throwable e) {
+                            if (e.getCause() instanceof InterruptedException) {
+                                return;
+                            }
+                            if (!instance.getLifecycleService().isRunning()) {
+                                return;
+                            }
+                            logger.warning(e.getClass().getName() + ": " + e.getMessage(), e);
+                        }
+                    }
+                }
+
+                private Object newKey(Random rand) {
+                    return keyRange <= 0 || keyRange == Integer.MAX_VALUE
+                        ? rand.nextLong() : rand.nextInt(keyRange);
+                }
+            });
+        }
+    }
+
+    private void startPrintStats() {
+        new Thread() {
+            {
+                setDaemon(true);
+                setName("PrintStats." + instance.getName());
+            }
+
+            public void run() {
+                long terminate = System.currentTimeMillis() + duration;
+                if (terminate < 0) {
+                    terminate = Long.MAX_VALUE;
+                }
+                final AbstractHistogram totalHistogram =
+                    new Histogram(1, TimeUnit.MINUTES.toNanos(1), 3);
+                while (true) {
+                    try {
+                        long start = System.currentTimeMillis();
+                        if (start >= terminate) {
+                            live.set(false);
+                            ex.shutdown();
+                            ex.awaitTermination(2, TimeUnit.MINUTES);
+                            instance.shutdown();
+                            break;
+                        }
+                        Thread.sleep(STATS_SECONDS * 1000);
+                        long end = System.currentTimeMillis();
+                        long interval = end - start;
+
+                        totalHistogram.reset();
+                        long getsNow = 0;
+                        long putsNow = 0;
+                        long removesNow = 0;
+
+                        for (int i = 0; i < threadCount; i++) {
+                            Stats stats = allStats[i];
+                            getsNow += stats.gets.getAndSet(0);
+                            putsNow += stats.puts.getAndSet(0);
+                            removesNow += stats.removes.getAndSet(0);
+
+                            totalHistogram.add(stats.histogram);
+                            stats.histogram.reset();
+                        }
+                        long totalOps = getsNow + putsNow + removesNow;
+
+                        totalHistogram.reestablishTotalCount();
+                        totalHistogram.outputPercentileDistribution(System.out, 1, 1000d);
+
+                        System.out.println();
+                        System.out.println(
+                            "total-ops= " + (totalOps * 1000 / interval) + ", gets:" + (
+                                getsNow * 1000 / interval)
+                                + ", puts:" + (putsNow * 1000 / interval) + ", removes:" + (
+                                removesNow * 1000 / interval));
+                        System.out.println();
+                        System.out.println();
+                    } catch (InterruptedException ignored) {
+                        return;
+                    }
+                }
+            }
+        }.start();
+    }
+
+    private static class Stats {
+        final AbstractHistogram histogram = new AtomicHistogram(1, TimeUnit.MINUTES.toNanos(1), 3);
+        final AtomicInteger gets = new AtomicInteger();
+        final AtomicInteger puts = new AtomicInteger();
+        final AtomicInteger removes = new AtomicInteger();
+    }
+
+    private void printVariables() {
+        logger.info("Starting Test with ");
+        logger.info("Thread Count: " + threadCount);
+        logger.info("Get Percentage: " + getPercentage);
+        logger.info("Put Percentage: " + putPercentage);
+        logger.info("Remove Percentage: " + (100 - (putPercentage + getPercentage)));
+    }
+}
---
+++ enterprise/hd-memory-client-server/hd-memory-examples-server/src/main/java/EnterpriseCacheTestServer.java
@@ -0,0 +1,94 @@
+import com.hazelcast.config.Config;
+import com.hazelcast.config.NativeMemoryConfig;
+import com.hazelcast.config.XmlConfigBuilder;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.logging.ILogger;
+import com.hazelcast.memory.MemorySize;
+import com.hazelcast.memory.MemoryStats;
+import com.hazelcast.memory.MemoryUnit;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * A simple test of a cache.
+ */
+public class EnterpriseCacheTestServer {
+
+    private static final long STATS_SECONDS = 10;
+
+    private final HazelcastInstance instance;
+    private final MemoryStats memoryStats;
+    private final ILogger logger;
+    private final MemorySize memorySize;
+
+    static {
+        System.setProperty("hazelcast.version.check.enabled", "false");
+        System.setProperty("hazelcast.socket.bind.any", "false");
+        System.setProperty("java.net.preferIPv4Stack", "true");
+        System.setProperty("hazelcast.multicast.group", "224.33.55.79");
+    }
+
+    private EnterpriseCacheTestServer(String memory) throws IOException {
+        this.memorySize = MemorySize.parse(memory, MemoryUnit.GIGABYTES);
+
+        InputStream configInputStream = EnterpriseCacheTestServer.class.getResourceAsStream("/hazelcast-hd-memory.xml");
+        Config cfg = new XmlConfigBuilder(configInputStream).build();
+
+        NativeMemoryConfig memoryConfig = cfg.getNativeMemoryConfig();
+        if (!memoryConfig.isEnabled()) {
+            memoryConfig.setSize(memorySize).setEnabled(true);
+            memoryConfig.setAllocatorType(NativeMemoryConfig.MemoryAllocatorType.POOLED);
+        }
+
+        instance = Hazelcast.newHazelcastInstance(cfg);
+        memoryStats = MemoryStatsUtil.getMemoryStats(instance);
+        logger = instance.getLoggingService().getLogger(EnterpriseCacheTestServer.class);
+    }
+
+    public static void main(String[] input) throws Exception {
+        String memory = "3";
+        boolean master = false;
+        if (input != null && input.length > 0) {
+            memory = input[0];
+            if(input.length > 1 && input[1].equals("master"))
+                master = true;
+        }
+
+        EnterpriseCacheTestServer test = new EnterpriseCacheTestServer(memory);
+        test.start();
+    }
+
+    private void start() throws InterruptedException {
+        printVariables();
+        startPrintStats();
+    }
+
+    private void startPrintStats() {
+        new Thread() {
+            {
+                setDaemon(true);
+                setName("PrintStats." + instance.getName());
+            }
+
+            public void run() {
+                while (true) {
+                    try {
+                        Thread.sleep(STATS_SECONDS * 1000);
+                        System.out.println("");
+                        System.out.println(memoryStats);
+                    } catch (InterruptedException ignored) {
+                        return;
+                    }
+                }
+            }
+        }.start();
+    }
+
+    private void printVariables() {
+        logger.info("Starting Test with ");
+        logger.info("Memory: " + memorySize.toPrettyString());
+    }
+
+}
