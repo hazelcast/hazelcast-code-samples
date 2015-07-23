99e5f972301ab87abd879e7faf07690642756c82
alarmnummer <peter.veentjer@hazelcast.com>
2015-07-23 06:20:52 +0300

    Added ReliableTopic example

--- distributed-collections/ringbuffer/src/main/java/Reader.java
+++ distributed-collections/ringbuffer/src/main/java/Reader.java
@@ -8,6 +8,8 @@ public class Reader {
         HazelcastInstance hz = Hazelcast.newHazelcastInstance();
 
         Ringbuffer<Long> rb = hz.getRingbuffer("rb");
+        // we start from the oldest item.
+        // if you want to start from the next item, call rb.tailSequence()+1
         long sequence = rb.headSequence();
         System.out.println("Start reading from: " + sequence);
         while (true) {
---
+++ distributed-topic/reliable-topic/src/main/java/SubscribedMember.java
@@ -0,0 +1,23 @@
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.ITopic;
+import com.hazelcast.core.Message;
+import com.hazelcast.core.MessageListener;
+
+import java.util.Date;
+import java.util.Random;
+
+public class SubscribedMember {
+
+    public static void main(String[] args) throws InterruptedException {
+        HazelcastInstance hz = Hazelcast.newHazelcastInstance();
+        ITopic<Long> topic = hz.getReliableTopic("sometopic");
+        topic.addMessageListener(new MessageListenerImpl());
+    }
+
+    private static class MessageListenerImpl implements MessageListener<Long> {
+        public void onMessage(Message<Long> m) {
+            System.out.println("Received: " + m.getMessageObject());
+        }
+    }
+}
