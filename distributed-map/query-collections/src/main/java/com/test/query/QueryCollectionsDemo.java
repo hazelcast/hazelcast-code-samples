ff6b93f475677ab3a31cc65ebc3625ef06734c1f
Tom Bujok <tom@hazelcast.com>
2015-10-26 23:27:00 +0100

    Code sample for querying collections

---
+++ distributed-map/query-collections/src/main/java/com/test/query/QueryCollectionsDemo.java
@@ -0,0 +1,54 @@
+package com.test.query;
+
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IMap;
+import com.hazelcast.query.SqlPredicate;
+
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+
+import static com.test.query.QueryCollectionsDemo.Limb.limb;
+
+public class QueryCollectionsDemo {
+
+    public static void main(String[] args) {
+        HazelcastInstance hz = Hazelcast.newHazelcastInstance();
+        IMap<String, Person> map = hz.getMap("map");
+
+        map.put("1", new Person("peter", limb("left-hand"), limb("right-hand")));
+        map.put("2", new Person("hans", limb("left-leg"), limb("right-leg")));
+
+        Set<Person> employees = (Set<Person>) map.values(new SqlPredicate("limbs[any].name == right-leg"));
+        System.out.println("People:" + employees);
+    }
+
+    public static class Person implements Serializable {
+        final String name;
+        final List<Limb> limbs;
+
+        private Person(String name, Limb... limbs) {
+            this.name = name;
+            this.limbs = Arrays.asList(limbs);
+        }
+
+        @Override
+        public String toString() {
+            return "Person{name='" + name + '}';
+        }
+    }
+
+    public static class Limb implements Serializable {
+        final String name;
+
+        public Limb(String name) {
+            this.name = name;
+        }
+
+        public static Limb limb(String name) {
+            return new Limb(name);
+        }
+    }
+}
