53c9caf41097130fc3270c16c734925078349eb1
Ensar Basri Kahveci <ebkahveci@gmail.com>
2015-11-17 13:58:36 +0200

    Add code samples for task execution on lite members

---
+++ distributed-executor/executing-on-lite-members/src/main/java/ComputationHeavyTask.java
@@ -0,0 +1,19 @@
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.HazelcastInstanceAware;
+
+import java.io.Serializable;
+import java.util.concurrent.Callable;
+
+public class ComputationHeavyTask implements
+        Callable<Integer>, Serializable, HazelcastInstanceAware {
+    private transient HazelcastInstance hz;
+
+    public void setHazelcastInstance(HazelcastInstance hz) {
+        this.hz = hz;
+    }
+
+    public Integer call() throws Exception {
+        System.out.println("Running a computation heavy task on " + hz.getCluster().getLocalMember());
+        return 0;
+    }
+}
