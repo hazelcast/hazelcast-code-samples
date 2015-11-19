d3fc0d2f39d07dda6135965f060ecbb8c23d729d
Viktor Gamov <gamussa@gmail.com>
2015-11-18 23:32:59 -0500

    Spring Caching Java Configuration example
    - 2 spring applications - xml and java config
    - added "slow" method emulation

---
+++ hazelcast-integration/spring-jcache/src/main/java/com/hazelcast/spring/jcache/SpringJCacheXMLConfig.java
@@ -0,0 +1,20 @@
+package com.hazelcast.spring.jcache;
+
+import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.core.Hazelcast;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import static com.hazelcast.spring.jcache.DummyBeanTest.doInvocation;
+
+public class SpringJCacheXMLConfig {
+    public static void main(String[] args)
+        throws InterruptedException {
+        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+
+        doInvocation(context);
+
+        HazelcastClient.shutdownAll();
+        Hazelcast.shutdownAll();
+    }
+}
