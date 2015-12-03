f00ef2e5cf2a0f1dd37f4abdbac1675fc247a2b4
donnerbart <github@thunderphreak.de>
2015-12-03 12:41:42 +0100

    Cleanup of PartitionLostListenerUsage.

--- jcache/src/main/java/com/hazelcast/examples/listener/PartitionLostListenerUsage.java
+++ jcache/src/main/java/com/hazelcast/examples/listener/PartitionLostListenerUsage.java
@@ -1,6 +1,5 @@
 package com.hazelcast.examples.listener;
 
-
 import com.hazelcast.cache.ICache;
 import com.hazelcast.cache.impl.HazelcastServerCachingProvider;
 import com.hazelcast.cache.impl.event.CachePartitionLostEvent;
@@ -12,48 +11,61 @@ import com.hazelcast.core.HazelcastInstance;
 
 import javax.cache.Cache;
 import javax.cache.CacheManager;
+import java.util.concurrent.CountDownLatch;
 
 import static com.hazelcast.cache.impl.HazelcastServerCachingProvider.createCachingProvider;
 
 public class PartitionLostListenerUsage {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+
+        final CountDownLatch latch = new CountDownLatch(1);
 
         String cacheName1 = "myCache1";
         String cacheName2 = "myCache2";
 
-        HazelcastInstance serverInstance1  = Hazelcast.newHazelcastInstance(new Config());
-        HazelcastInstance serverInstance2  = Hazelcast.newHazelcastInstance(new Config());
+        HazelcastInstance serverInstance1 = Hazelcast.newHazelcastInstance(new Config());
+        HazelcastInstance serverInstance2 = Hazelcast.newHazelcastInstance(new Config());
 
-        final HazelcastServerCachingProvider cachingProvider = createCachingProvider(serverInstance1);
-        final CacheManager cacheManager = cachingProvider.getCacheManager();
+        HazelcastServerCachingProvider cachingProvider = createCachingProvider(serverInstance1);
+        CacheManager cacheManager = cachingProvider.getCacheManager();
 
-        final CacheConfig<Integer, String> config1 = new CacheConfig<Integer, String>();
-        config1.setBackupCount(0); //might lose data if any node crashes
-        final Cache<Integer, String> cache1 = cacheManager.createCache(cacheName1, config1);
+        CacheConfig<Integer, String> config1 = new CacheConfig<Integer, String>();
+        // might lose data if any node crashes
+        config1.setBackupCount(0);
+        Cache<Integer, String> cache1 = cacheManager.createCache(cacheName1, config1);
+
         cache1.put(1, "Berlin");
-        final ICache iCache1 = cache1.unwrap(ICache.class);
 
+        ICache iCache1 = cache1.unwrap(ICache.class);
         iCache1.addPartitionLostListener(new CachePartitionLostListener() {
             @Override
             public void partitionLost(CachePartitionLostEvent cachePartitionLostEvent) {
                 System.out.println(cachePartitionLostEvent);
+                latch.countDown();
             }
         });
 
-        final CacheConfig<Integer, String> config2 = new CacheConfig<Integer, String>();
-        config2.setBackupCount(1); //keeps its data if a single node crashes
-        final Cache<Integer, String> cache2 = cacheManager.createCache(cacheName2, config2);
-        cache1.put(1, "Berlin");
-        final ICache iCache2 = cache2.unwrap(ICache.class);
+        CacheConfig<Integer, String> config2 = new CacheConfig<Integer, String>();
+        // keeps its data if a single node crashes
+        config2.setBackupCount(1);
+        Cache<Integer, String> cache2 = cacheManager.createCache(cacheName2, config2);
 
+        cache1.put(1, "Berlin");
+
+        ICache iCache2 = cache2.unwrap(ICache.class);
         iCache2.addPartitionLostListener(new CachePartitionLostListener() {
             @Override
             public void partitionLost(CachePartitionLostEvent cachePartitionLostEvent) {
-                System.out.println("This line will not be printed! " + cachePartitionLostEvent);
+                System.err.println("This line should not be printed! " + cachePartitionLostEvent);
             }
         });
 
+        System.out.println("Terminating second Hazelcast instance...");
         serverInstance2.getLifecycleService().terminate();
+
+        // wait for a least one CachePartitionLostEvent
+        latch.await();
+        Hazelcast.shutdownAll();
     }
 }
