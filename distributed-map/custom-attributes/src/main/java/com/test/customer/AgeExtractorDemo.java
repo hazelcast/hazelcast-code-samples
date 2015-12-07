5d9bd7c48dab822a907307fb6c4a50933c1ed559
donnerbart <github@thunderphreak.de>
2015-12-07 11:45:22 +0100

    Cleanup of custom attributes demos.

--- distributed-map/custom-attributes/src/main/java/com/test/customer/AgeExtractorDemo.java
+++ distributed-map/custom-attributes/src/main/java/com/test/customer/AgeExtractorDemo.java
@@ -12,15 +12,16 @@ public class AgeExtractorDemo {
 
     public static void main(String[] args) throws ParseException {
         HazelcastInstance hz = Hazelcast.newHazelcastInstance();
-        IMap<String, Customer> map = hz.getMap("people");
+        IMap<Integer, Customer> map = hz.getMap("customers");
 
-        map.put("1", new Customer("james", "bond", 1975));
-        map.put("2", new Customer("eathen", "hunt", 1965));
-        map.put("3", new Customer("roger", "moore", 1995));
+        map.put(1, new Customer("James", "Bond", 1975));
+        map.put(2, new Customer("Eathen", "Hunt", 1965));
+        map.put(3, new Customer("Roger", "Moore", 1995));
 
-        // We're using a custom attribute 'age' which is provided by the 'AgeExtractor'
-        Set<Customer> employees = (Set<Customer>) map.values(new SqlPredicate("age < 50"));
-        System.out.println("Employees:" + employees);
+        // we're using a custom attribute 'age' which is provided by the 'AgeExtractor'
+        Set<Customer> customers = (Set<Customer>) map.values(new SqlPredicate("age < 50"));
+        System.out.println("Customers: " + customers);
+
+        Hazelcast.shutdownAll();
     }
-
 }
--- distributed-map/custom-attributes/src/main/java/com/test/customer/Customer.java
+++ distributed-map/custom-attributes/src/main/java/com/test/customer/Customer.java
@@ -19,12 +19,11 @@ public class Customer implements Serializable {
 
     @Override
     public String toString() {
-        return "Customer{" +
-                "id='" + id + '\'' +
-                ", name='" + name + '\'' +
-                ", surname='" + surname + '\'' +
-                ", yearOfBirth=" + yearOfBirth +
-                '}';
+        return "Customer{"
+                + "id='" + id + '\''
+                + ", name='" + name + '\''
+                + ", surname='" + surname + '\''
+                + ", yearOfBirth=" + yearOfBirth
+                + '}';
     }
-
 }
