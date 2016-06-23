c9bf28b363ea1a567211e491f6058cb7122af48f
Emrah Kocaman <emrahkocaman@gmail.com>
2016-06-23 15:58:14 +0300

    remove -server flags, fixes https://github.com/hazelcast/hazelcast/issues/8445

--- demo/src/main/resources/console.bat
+++ demo/src/main/resources/console.bat
@@ -1,1 +1,1 @@
-java -server -cp ../target/lib/hazelcast-all-${hazelcast.version}.jar com.hazelcast.console.ConsoleApp
+java -cp ../target/lib/hazelcast-all-${hazelcast.version}.jar com.hazelcast.console.ConsoleApp
