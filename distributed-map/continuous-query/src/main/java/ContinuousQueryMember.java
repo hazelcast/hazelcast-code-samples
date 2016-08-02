dcea2b8508c8a0df9381ff972defe8dc51ff9096
Vassilis Bekiaris <vbekiaris@gmail.com>
2016-08-02 12:24:01 +0300

    Updated continuous query sample to use natural filtering event types introduced in 3.7

--- distributed-map/continuous-query/src/main/java/ContinuousQueryMember.java
+++ distributed-map/continuous-query/src/main/java/ContinuousQueryMember.java
@@ -1,3 +1,4 @@
+import com.hazelcast.config.Config;
 import com.hazelcast.core.EntryEvent;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
@@ -11,10 +12,55 @@ import com.hazelcast.map.listener.MapClearedListener;
 import com.hazelcast.map.listener.MapEvictedListener;
 import com.hazelcast.query.SqlPredicate;
 
+/**
+ * Registers an entry listener with a predicate to continuously query map data as it is updated.
+ * Since Hazelcast version 3.7, initializing your Hazelcast instance to publish natural filtering event types (setting property
+ * {@code hazelcast.map.entry.filtering.natural.event.types} to {@code true}) will change how
+ * entry listeners with predicates are notified about map entry value updates to accommodate the continuous
+ * querying use case. The following table compares how a listener is notified about an update to a map entry value
+ * under the default backwards-compatible Hazelcast behavior (when the property above is not set
+ * or is set to {@code false}) versus when set to {@code true}:
+ * <table>
+ *     <tr>
+ *         <th>&nbsp;</th>
+ *         <th>Default</th>
+ *         <th>hazelcast.map.entry.filtering.natural.event.types = true</th>
+ *     </tr>
+ *     <tr>
+ *         <td>When old value matches predicate,<br/>new value does not match predicate</td>
+ *         <td>No event is delivered to entry listener</td>
+ *         <td>{@code REMOVED} event is delivered to entry listener</td>
+ *     </tr>
+ *     <tr>
+ *         <td>When old value matches predicate,<br/>new value matches predicate</td>
+ *         <td>{@code UPDATED} event is delivered to entry listener</td>
+ *         <td>{@code UPDATED} event is delivered to entry listener</td>
+ *     </tr>
+ *     <tr>
+ *         <td>When old value does not match predicate,<br/>new value does not match predicate</td>
+ *         <td>No event is delivered to entry listener</td>
+ *         <td>No event is delivered to entry listener</td>
+ *     </tr>
+ *     <tr>
+ *         <td>When old value does not match predicate,<br/>new value matches predicate</td>
+ *         <td>{@code UPDATED} event is delivered to entry listener</td>
+ *         <td>{@code ADDED} event is delivered to entry listener</td>
+ *     </tr>
+ * </table>
+ *
+ * Conceptually, enabling the natural filtering event types property produces events reflecting how map entries are
+ * added, updated or removed with regards to the space of values matching the predicate.
+ *
+ * @see com.hazelcast.map.impl.event.QueryCacheNaturalFilteringStrategy
+ * @see com.hazelcast.map.impl.event.DefaultEntryEventFilteringStrategy
+ * @see com.hazelcast.map.impl.event.MapEventPublisherImpl
+ */
 public class ContinuousQueryMember {
 
     public static void main(String[] args) {
-        HazelcastInstance hz = Hazelcast.newHazelcastInstance();
+        Config config = new Config();
+        config.setProperty("hazelcast.map.entry.filtering.natural.event.types", "true");
+        HazelcastInstance hz = Hazelcast.newHazelcastInstance(config);
         IMap<String, String> map = hz.getMap("map");
         map.addEntryListener(new MyEntryListener(),
                 new SqlPredicate("name=peter"), true);
