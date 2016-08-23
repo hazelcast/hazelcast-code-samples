8ea1327b04728dbd1aece8470e81cfd47c8adf17
donnerbart <github@thunderphreak.de>
2016-08-23 15:11:37 +0200

    Added example for eviction-policy for IMap Near Cache.

---
+++ distributed-map/near-cache/src/main/java/NearCacheWithEvictionPolicy.java
@@ -0,0 +1,47 @@
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IMap;
+import com.hazelcast.monitor.NearCacheStats;
+
+public class NearCacheWithEvictionPolicy extends NearCacheSupport {
+
+    public static void main(String[] args) {
+        HazelcastInstance hz = Hazelcast.newHazelcastInstance();
+        IMap<Integer, Article> map = hz.getMap("articlesEvictionPolicy");
+
+        for (int i = 1; i <= 100; i++) {
+            map.put(1, new Article("foo" + i));
+        }
+        printNearCacheStats(map, "The put(1..100, article) calls have no effect on the empty Near Cache");
+
+        for (int i = 1; i <= 100; i++) {
+            map.get(i);
+        }
+        printNearCacheStats(map, "The first get(1..100) calls populate the Near Cache");
+
+        for (int i = 1; i <= 100; i++) {
+            map.get(i);
+        }
+        printNearCacheStats(map, "The second get(1..100) calls are served from the Near Cache");
+
+        map.put(101, new Article("bar"));
+        printNearCacheStats(map, "The put(101, article) call has no effect on the populated Near Cache");
+
+        map.get(101);
+        printNearCacheStats(map, "The first get(101) call triggers the eviction and population of the Near Cache");
+
+        waitForNearCacheEntryCount(map, 90);
+        printNearCacheStats(map, "The Near Cache has been evicted");
+
+        map.get(101);
+        printNearCacheStats(map, "The second get(101) call is served from the Near Cache");
+
+        Hazelcast.shutdownAll();
+    }
+
+    private static void printNearCacheStats(IMap<Integer, Article> map, String message) {
+        NearCacheStats stats = map.getLocalMapStats().getNearCacheStats();
+        System.out.printf("%s (%d entries, %d hits, %d misses)%n",
+                message, stats.getOwnedEntryCount(), stats.getHits(), stats.getMisses());
+    }
+}
