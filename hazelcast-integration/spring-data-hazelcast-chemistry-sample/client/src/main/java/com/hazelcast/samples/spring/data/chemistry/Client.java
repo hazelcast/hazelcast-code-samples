92ebb9ac7d32f2bd2d745bf2491ecddb732ad5d3
donnerbart <github@thunderphreak.de>
2016-10-20 01:51:50 +0200

    Fixed CheckStyle issues in Spring Data Hazelcast Chemistry client module.

--- hazelcast-integration/spring-data-hazelcast-chemistry-sample/client/src/main/java/com/hazelcast/samples/spring/data/chemistry/Client.java
+++ hazelcast-integration/spring-data-hazelcast-chemistry-sample/client/src/main/java/com/hazelcast/samples/spring/data/chemistry/Client.java
@@ -4,24 +4,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * <P>Run a Hazelcast client as a REST application. Data can be inspected
+ * Run a Hazelcast client as a REST application. Data can be inspected
  * using simple URL calls in a browser.
- * </P>
- * <P>Use <A HREF="http://localhost:8080/">http://localhost:8080/</A>
+ *
+ * Use <a href="http://localhost:8080/">http://localhost:8080/</a>
  * to discover what URLs are available.
- * </P>
  */
 @SpringBootApplication
 public class Client {
-	
-	/**
-	 * <P>Launch Spring Boot.
-	 * </P>
-	 * 
-     * @param args		  From the O/s to pass on          
-	 */
-	public static void main(String[] args) {
-		SpringApplication.run(Client.class, args);
-	}
-	
+
+    /**
+     * Launch Spring Boot.
+     *
+     * @param args From the O/s to pass on
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(Client.class, args);
+    }
 }
--- hazelcast-integration/spring-data-hazelcast-chemistry-sample/client/src/main/java/com/hazelcast/samples/spring/data/chemistry/ClientConfiguration.java
+++ hazelcast-integration/spring-data-hazelcast-chemistry-sample/client/src/main/java/com/hazelcast/samples/spring/data/chemistry/ClientConfiguration.java
@@ -1,31 +1,27 @@
 package com.hazelcast.samples.spring.data.chemistry;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
 import com.hazelcast.client.HazelcastClient;
 import com.hazelcast.client.config.ClientConfig;
 import com.hazelcast.client.config.XmlClientConfigBuilder;
 import com.hazelcast.core.HazelcastInstance;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 /**
- * <P>Spring beans specific to a Hazelcast client-side process.
- * </P>
+ * Spring beans specific to a Hazelcast client-side process.
  */
 @Configuration
 public class ClientConfiguration {
 
-	/**
-	 * <P>Create a Hazelcast client and wrap it as a Spring bean.
-	 * </P>
-	 * 
-	 * @return A Hazelcast client singleton
-	 */
-	@Bean
-	public HazelcastInstance hazelcastInstance() throws Exception {
-		ClientConfig clientConfig = new XmlClientConfigBuilder("hazelcast-client.xml").build();
-		
-		return HazelcastClient.newHazelcastClient(clientConfig);
-	}
+    /**
+     * Create a Hazelcast client and wrap it as a Spring bean.
+     *
+     * @return A Hazelcast client singleton
+     */
+    @Bean
+    public HazelcastInstance hazelcastInstance() throws Exception {
+        ClientConfig clientConfig = new XmlClientConfigBuilder("hazelcast-client.xml").build();
 
+        return HazelcastClient.newHazelcastClient(clientConfig);
+    }
 }
