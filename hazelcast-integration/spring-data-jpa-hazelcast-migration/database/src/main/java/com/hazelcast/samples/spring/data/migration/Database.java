2fb087f158a8407737fdb199c111d38ca1e6128b
donnerbart <github@thunderphreak.de>
2016-12-20 15:17:53 +0100

    Fixed CheckStyle errors in Spring Data JPA database module.

--- hazelcast-integration/spring-data-jpa-hazelcast-migration/database/src/main/java/com/hazelcast/samples/spring/data/migration/Database.java
+++ hazelcast-integration/spring-data-jpa-hazelcast-migration/database/src/main/java/com/hazelcast/samples/spring/data/migration/Database.java
@@ -4,29 +4,24 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.shell.Bootstrap;
 
 /**
- * <P>Use <A HREF="https://projects.spring.io/spring-shell">Spring Shell</A>
+ * Use <a href="https://projects.spring.io/spring-shell">Spring Shell</a>
  * for command line handling as it's more appropriate than
- * <A HREF="http://docs.spring.io/spring-boot/docs/current/reference/html/production-ready-remote-shell.html">CRaSH</A>
+ * <a href="http://docs.spring.io/spring-boot/docs/current/reference/html/production-ready-remote-shell.html">CRaSH</a>
  * although not as nicely integrated yet.
- * </P>
- * @see <A HREF="https://github.com/spring-projects/spring-shell/issues/34">
- * 	https://github.com/spring-projects/spring-shell/issues/34</A> 
+ *
+ * @see <a href="https://github.com/spring-projects/spring-shell/issues/34">
+ * https://github.com/spring-projects/spring-shell/issues/34</a>
  */
 @SpringBootApplication
 public class Database {
 
-	static {
-		/**
-		 * <P>File {@code import.sql} is loaded, but for readability
-		 * confirm commands are multi-line.
-		 * </P>
-		 */
-    	System.setProperty("hibernate.hbm2ddl.import_files_sql_extractor"
-   	    	  ,"org.hibernate.tool.hbm2ddl.MultipleLinesSqlCommandExtractor");
-	}
-	
-    public static void main(String[] args) throws Exception {
-    	Bootstrap.main(args);
+    static {
+        // File `import.sql` is loaded, but for readability confirm commands are multi-line
+        System.setProperty("hibernate.hbm2ddl.import_files_sql_extractor"
+                , "org.hibernate.tool.hbm2ddl.MultipleLinesSqlCommandExtractor");
     }
-    
+
+    public static void main(String[] args) throws Exception {
+        Bootstrap.main(args);
+    }
 }
--- hazelcast-integration/spring-data-jpa-hazelcast-migration/database/src/main/java/com/hazelcast/samples/spring/data/migration/DatabaseConfiguration.java
+++ hazelcast-integration/spring-data-jpa-hazelcast-migration/database/src/main/java/com/hazelcast/samples/spring/data/migration/DatabaseConfiguration.java
@@ -1,7 +1,5 @@
 package com.hazelcast.samples.spring.data.migration;
 
-import javax.sql.DataSource;
-
 import org.hsqldb.Server;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
@@ -10,59 +8,55 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
 import org.springframework.context.annotation.PropertySource;
 
+import javax.sql.DataSource;
+
 /**
- * <P>Create a running database server, and a
- * database inside it.
- * </P>
+ * Create a running database server, and a database inside it.
  */
 @Configuration
 @PropertySource("classpath:application.properties")
 public class DatabaseConfiguration {
 
-	@Value("${spring.datasource.password}")
-	private String password;
-	@Value("${spring.datasource.username}")
-	private String username;
-	@Value("${spring.datasource.url}")
-	private String url;
+    @Value("${spring.datasource.password}")
+    private String password;
+    @Value("${spring.datasource.username}")
+    private String username;
+    @Value("${spring.datasource.url}")
+    private String url;
 
-	/**
-	 * <P>The server can support multiple, but only
-	 * create one database, the first of ten possibles.
-	 * </P>
-	 * 
-	 * @return A running database server.
-	 */
-	@Bean
-	public Server server() {
-		String MYDATABASE = "mydatabase";
-		
-		Server server = new Server();
-		
-		server.setDatabaseName(0, MYDATABASE);
-		server.setDatabasePath(0, "file:" + MYDATABASE + "db");
-		
-		server.start();
-		
-		return server;
-	}
+    /**
+     * The server can support multiple, but only create one database, the first of ten possibles.
+     *
+     * @return A running database server.
+     */
+    @Bean
+    public Server server() {
+        String myDataBase = "mydatabase";
 
-	
-	/**
-	 * <P>Create an explicit database to stop Spring Boot from trying.
-	 * Force creation after the server, to avoid dependency errors.
-	 * </P>
-	 * 
-	 * @return A datasource onto the database above
-	 */
-	@Bean
-	@DependsOn("server")
-	public DataSource dataSource() {
-		return DataSourceBuilder.create()
-				.url(this.url)
-				.username(this.username)
-				.password(this.password)
-				.build();
-	}
+        Server server = new Server();
 
+        server.setDatabaseName(0, myDataBase);
+        server.setDatabasePath(0, "file:" + myDataBase + "db");
+
+        server.start();
+
+        return server;
+    }
+
+    /**
+     * Create an explicit database to stop Spring Boot from trying.
+     *
+     * Force creation after the server, to avoid dependency errors.
+     *
+     * @return A datasource onto the database above
+     */
+    @Bean
+    @DependsOn("server")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create()
+                .url(this.url)
+                .username(this.username)
+                .password(this.password)
+                .build();
+    }
 }
