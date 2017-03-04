c3ae5e38eab26b1af8eaabbe37180909b2a6fe0a
Neil Stevenson <neilstevenson@users.noreply.github.com>
2017-03-04 21:52:44 +0000

    Add Eureka Server

---
+++ hazelcast-integration/springboot-eureka-partition-groups/eureka-server/src/main/java/com/hazelcast/samples/eureka/partition/groups/EurekaServer.java
@@ -0,0 +1,27 @@
+package com.hazelcast.samples.eureka.partition.groups;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * <P>
+ * Run with "{@code java -jar eureka-server-0.1-SNAPSHOT.jar}"
+ * </P>
+ * <P>
+ * Eureka server will be available from
+ * <a href="http://localhost:8761/">http://localhost:8761/</a>
+ * </P>
+ * <P>
+ * Eureka should really be clustered.
+ * </P>
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServer {
+
+        public static void main(String[] args) {
+                SpringApplication.run(EurekaServer.class, args);
+        }
+        
+}
