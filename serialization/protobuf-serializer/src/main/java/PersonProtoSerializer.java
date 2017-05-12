e0bf2d6116e1889444fc396a1c37938b8dcf2dea
Viktor Gamov <viktor@hazelcast.com>
2017-05-12 15:25:35 -0400

    PersonProto serializer and some predicates example

---
+++ serialization/protobuf-serializer/src/main/java/PersonProtoSerializer.java
@@ -0,0 +1,34 @@
+import com.hazelcast.nio.ObjectDataInput;
+import com.hazelcast.nio.ObjectDataOutput;
+import com.hazelcast.nio.serialization.StreamSerializer;
+import com.hazelcast.serialization.PersonProtos;
+
+import java.io.IOException;
+
+/**
+ * Serializer for Person class generated from Protobuf compiler
+ *
+ * @author Viktor Gamov on 5/12/17.
+ *         Twitter: @gamussa
+ */
+public class PersonProtoSerializer implements StreamSerializer<PersonProtos.Person> {
+    @Override
+    public void write(ObjectDataOutput out, PersonProtos.Person person) throws IOException {
+        out.writeByteArray(person.toByteArray());
+    }
+
+    @Override
+    public PersonProtos.Person read(ObjectDataInput in) throws IOException {
+        return PersonProtos.Person.parseFrom(in.readByteArray());
+    }
+
+    @Override
+    public int getTypeId() {
+        return 42;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
