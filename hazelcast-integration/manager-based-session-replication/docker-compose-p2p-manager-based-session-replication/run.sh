ce02162076c6fb45b9ae800586f8ab7b6d881aa9
Bilal Demirci <bilaldmrc@gmail.com>
2017-06-28 12:05:18 +0300

    updated docker-compose.yml files

--- hazelcast-integration/manager-based-session-replication/docker-compose-p2p-manager-based-session-replication/run.sh
+++ hazelcast-integration/manager-based-session-replication/docker-compose-p2p-manager-based-session-replication/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 curl https://oss.sonatype.org/content/repositories/releases/com/hazelcast/hazelcast-all/3.8.2/hazelcast-all-3.8.2.jar > hazelcast-all-3.8.2.jar
-curl https://oss.sonatype.org/content/repositories/releases/com/hazelcast/hazelcast-tomcat7-sessionmanager/1.1.1/hazelcast-tomcat7-sessionmanager-1.1.1.jar > hazelcast-tomcat7-sessionmanager-1.1.1.jar
+curl https://oss.sonatype.org/content/repositories/releases/com/hazelcast/hazelcast-tomcat85-sessionmanager/1.1.1/hazelcast-tomcat85-sessionmanager-1.1.1.jar > hazelcast-tomcat85-sessionmanager-1.1.1.jar
 docker-compose up
