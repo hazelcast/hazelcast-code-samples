512ae1f24f2cd447cf9cf9d68182bf91405bd152
donnerbart <github@thunderphreak.de>
2017-08-18 13:51:49 +0200

    Fixed cluster module and activated it

--- cluster/src/main/java/PromoteLiteMember.java
+++ cluster/src/main/java/PromoteLiteMember.java
@@ -1,5 +1,7 @@
 import com.hazelcast.config.Config;
+import com.hazelcast.core.Cluster;
 import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
 
 public class PromoteLiteMember {
 
