8c84553f71756832701a23930f2a253fd4d54a0e
Neil Stevenson <neilstevenson@users.noreply.github.com>
2017-10-27 15:52:54 +0100

    Initial

---
+++ jcache1.1/times-table/jcache-standard/src/main/java/com/hazelcast/samples/jcache/timestable/CLI.java
@@ -0,0 +1,17 @@
+package com.hazelcast.samples.jcache.timestable;
+
+/**
+ * <p>Command line processor
+ * </p>
+ */
+public class CLI {
+
+    /**
+     * <p>Process stdin.
+     * </p>
+     */
+    public void process() {
+        // TODO Auto-generated method stub
+    }
+
+}
