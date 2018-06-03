29b8c58b8eeeac885bfdccf10512afe4b5c4c95f
dbrimley <dave.brimley@gmail.com>
2018-06-03 20:10:11 +0100

    Initial commit of JAAS Security
    Initial commit
    
    Java classes
    
    Fixed license issues
    
    Change URL in README.md for obtaining license
    
    ignore .java-version from jenv

---
+++ enterprise/jaas-security/src/main/java/Member.java
@@ -0,0 +1,44 @@
+import com.hazelcast.client.ClientEndpoint;
+import com.hazelcast.config.Config;
+import com.hazelcast.config.XmlConfigBuilder;
+import com.hazelcast.core.Client;
+import com.hazelcast.core.ClientListener;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+
+import java.io.InputStream;
+
+import static com.hazelcast.examples.helper.LicenseUtils.ENTERPRISE_LICENSE_KEY;
+
+/**
+ * You have to set your Hazelcast Enterprise license key to make this code sample work.
+ * Please have a look at {@link com.hazelcast.examples.helper.LicenseUtils} for details.
+ */
+public class Member {
+
+    public static void main(String args[]){
+
+        InputStream inputSteam = Member.class.getResourceAsStream("hazelcast.xml");
+        XmlConfigBuilder configBuilder = new XmlConfigBuilder(inputSteam);
+        Config config = configBuilder.build();
+
+        // Now set the license key.
+        config.setLicenseKey(ENTERPRISE_LICENSE_KEY);
+
+        HazelcastInstance hz = Hazelcast.newHazelcastInstance(config);
+        System.out.println("Hazelcast Member instance is running!");
+
+        hz.getClientService().addClientListener(new ClientListener() {
+
+            public void clientConnected(Client client) {
+                ClientEndpoint clientEndpoint = (ClientEndpoint) client;
+                System.out.println("Connected");
+            }
+
+            public void clientDisconnected(Client client) {
+
+            }
+        });
+    }
+
+}
