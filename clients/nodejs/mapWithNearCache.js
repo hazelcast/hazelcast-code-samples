321308a1bfec4866c043810e3b6920ffdb897890
Jaromir Hamala <jaromir.hamala@gmail.com>
2018-09-27 16:58:31 +0200

    Cleanup

--- clients/nodejs/mapWithNearCache.js
+++ clients/nodejs/mapWithNearCache.js
@@ -1,41 +1,28 @@
-/**
- * http://usejsdoc.org/
- */
 const HazelcastClient = require('hazelcast-client').Client;
 const Config = require('hazelcast-client').Config;
-let listener = require('./listener');
-
-//let config = new Config.ClientConfig();
-//config.networkConfig.addresses = [{host: '127.0.0.1', port: '5701'}];
+const listener = require('./listener');
 
 
-
-let initConfig = (nearCache) => {
-	  let config = new Config.ClientConfig();
+const initConfig = (nearCache) => {
+      const config = new Config.ClientConfig();
 	  config.networkConfig.addresses = ['127.0.0.1:5701'];
 
-
-	  //region NearCache
 	  if (nearCache) {
-	    let orgsNearCacheConfig = new Config.NearCacheConfig();
+        const orgsNearCacheConfig = new Config.NearCacheConfig();
 	    orgsNearCacheConfig.invalidateOnChange = true;
 	    orgsNearCacheConfig.name = 'my-distributed-map';
 
-	    let ncConfigs = {};
+	    const ncConfigs = {};
 	    ncConfigs[orgsNearCacheConfig.name] = orgsNearCacheConfig;
 	    config.nearCacheConfigs = ncConfigs;
 	  }
-	  process.stdout.write("Config Start ==> \n");
-	//  process.stdout.write((JSON.stringify(config, null, 2)));
-	  process.stdout.write("Config End  ==> \n");
 	  return config;
-	  //endregion
 	};
 
 
 
 HazelcastClient.newHazelcastClient(initConfig(true)).then((client) => {
-    let map = client.getMap('my-distributed-map');
+    const map = client.getMap('my-distributed-map');
     map.addEntryListener(listener, undefined, true)
         .then(() => map.put('key', 'value'))
         .then(() => map.get('key'))
