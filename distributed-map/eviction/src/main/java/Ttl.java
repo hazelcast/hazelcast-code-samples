31981fedfee88cbadbe2ce85c92ca21b34c706df
Thomas Kountis <tkountis@gmail.com>
2018-10-01 16:04:17 +0100

    Add samples for TTL & MaxIdle per Map entry (#267)

---
+++ distributed-map/eviction/src/main/java/Ttl.java
@@ -0,0 +1,31 @@
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IMap;
+
+import java.util.Map;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+public class Ttl {
+
+    public static void main(String[] args)
+            throws InterruptedException {
+        HazelcastInstance hazelcastInstance = Hazelcast.newHazelcastInstance();
+        Map<Integer, String> map = hazelcastInstance.getMap("default");
+
+        ((IMap<Integer, String>) map).put(1, "Number One", 5, SECONDS);
+        System.out.println("Entry expiration: " + ((IMap<Integer, String>) map).getEntryView(1).getExpirationTime());
+
+        // Entry expires
+        Thread.sleep(SECONDS.toMillis(5));
+        if (map.get(1) != null) {
+            System.err.println("Entry is still alive!");
+            System.exit(-1);
+            return;
+        }
+
+        System.out.println("Entry expired!");
+        System.exit(0);
+    }
+
+}
