a07f24afb11c866cc0d424175550b4ae78804585
hasancelik <hasan@hazelcast.com>
2019-08-29 15:10:08 +0300

    fix checkstyle errors

--- jcache/src/main/java/com/hazelcast/examples/splitbrain/custom/AbstractCacheSplitBrainSampleWithCustomCacheMergePolicy.java
+++ jcache/src/main/java/com/hazelcast/examples/splitbrain/custom/AbstractCacheSplitBrainSampleWithCustomCacheMergePolicy.java
@@ -1,6 +1,5 @@
 package com.hazelcast.examples.splitbrain.custom;
 
-import com.hazelcast.cache.CacheEntryView;
 import com.hazelcast.nio.ObjectDataInput;
 import com.hazelcast.nio.ObjectDataOutput;
 import com.hazelcast.spi.merge.MergingValue;
