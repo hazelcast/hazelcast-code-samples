67b9437c310095e4c9a2618f3d4e5de1d0b5e39d
Sergey Sitnikov <serge.sitnikov@gmail.com>
2019-12-23 19:09:23 +0700

    Add bitmap index code sample

--- distributed-map/index/src/main/java/Person.java
+++ distributed-map/index/src/main/java/Person.java
@@ -4,8 +4,24 @@ public class Person implements Serializable {
 
     private final Name name;
 
-    Person(Name name) {
+    private final int[] habits;
+
+    public Person(Name name) {
         this.name = name;
+        this.habits = null;
+    }
+
+    public Person(Name name, int[] habits) {
+        this.name = name;
+        this.habits = habits;
+    }
+
+    public Name getName() {
+        return name;
+    }
+
+    public int[] getHabits() {
+        return habits;
     }
 
     @Override
