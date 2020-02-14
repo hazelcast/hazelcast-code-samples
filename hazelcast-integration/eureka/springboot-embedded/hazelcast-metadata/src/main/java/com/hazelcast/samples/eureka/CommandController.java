bccfb0223f791ccfe1f0a76a1dc69b65b371f724
Rafał Leszko <rafal@hazelcast.com>
2020-02-14 12:39:17 +0100

    Update eureka sample 4.0 (#385)
    * Update Eureka Code Sample for 4.0

--- hazelcast-integration/eureka/springboot-embedded/hazelcast-metadata/src/main/java/com/hazelcast/samples/eureka/CommandController.java
+++ hazelcast-integration/eureka/springboot-embedded/hazelcast-metadata/src/main/java/com/hazelcast/samples/eureka/CommandController.java
@@ -1,7 +1,7 @@
 package com.hazelcast.samples.eureka;
 
 import com.hazelcast.core.HazelcastInstance;
-import com.hazelcast.core.IMap;
+import com.hazelcast.map.IMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
