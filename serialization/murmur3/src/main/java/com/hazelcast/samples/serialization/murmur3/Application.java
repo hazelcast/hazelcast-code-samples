ca0c01842b482eff2ddea9a57526ad249e9d6ca3
Neil Stevenson <neilstevenson@users.noreply.github.com>
2020-06-24 09:20:10 +0100

    Checkstyle

--- serialization/murmur3/src/main/java/com/hazelcast/samples/serialization/murmur3/Application.java
+++ serialization/murmur3/src/main/java/com/hazelcast/samples/serialization/murmur3/Application.java
@@ -39,7 +39,7 @@ public class Application {
 
         // Print partition count
         System.out.println("hazelcast.partition.count==" + properties.getProperty("hazelcast.partition.count"));
-        
+
         Config config = new Config();
         config.setProperties(properties);
 
@@ -49,19 +49,19 @@ public class Application {
             HazelcastInstance hazelcastInstance = Hazelcast.newHazelcastInstance(config);
             int size = hazelcastInstance.getCluster().getMembers().size();
             if (size == (i + 1)) {
-            	System.err.printf("Node '%s' in cluster, size now %d%n", hazelcastInstance.getName(), size);
+                System.err.printf("Node '%s' in cluster, size now %d%n", hazelcastInstance.getName(), size);
             } else {
-            	System.err.println("************************************************");
-            	System.err.printf("Node '%s' not connected, turn on logging and try again%n", hazelcastInstance.getName());
-            	System.err.println("************************************************");
+                System.err.println("************************************************");
+                System.err.printf("Node '%s' not connected, turn on logging and try again%n", hazelcastInstance.getName());
+                System.err.println("************************************************");
             }
         }
-        
+
         HazelcastInstance anyInstance = Hazelcast.getAllHazelcastInstances().iterator().next();
         IExecutorService executorService = anyInstance.getExecutorService("default");
         MyCallable myCallable = new MyCallable();
         IMap iMap = anyInstance.getMap(MAP_NAME);
-        
+
         // Predict and validate the placement of the first 25 integer keys.
         for (int i = 0 ; i < 25; i++) {
             byte[] value = new byte[i];
@@ -71,27 +71,27 @@ public class Application {
             int[] prediction = predictedPartitionId(i, PARTITION_COUNT);
             int predictedPartitionId = prediction[0];
             int predictedHashCode = prediction[1];
-            
+
             // Ask Hazelcast where the key actually is
             Future<String> node = executorService.submitToKeyOwner(myCallable, i);
             Partition partition = anyInstance.getPartitionService().getPartition(i);
 
             // See if they are the same!
             if (predictedPartitionId == partition.getPartitionId()) {
-            	System.out.printf("Key '%d', hashcode %d, is in partition %d, hosted currently by node %s%n",
-            			i, predictedHashCode, predictedPartitionId, node.get());
+                System.out.printf("Key '%d', hashcode %d, is in partition %d, hosted currently by node %s%n",
+                        i, predictedHashCode, predictedPartitionId, node.get());
             } else {
-            	System.err.println("************************************************");
-            	System.err.printf("ERROR: Key '%d'%n", i);
-            	System.err.printf("ERROR:   Predicted partition id '%d'%n", predictedPartitionId);
-            	System.err.printf("ERROR:   Actual partition id '%d'%n", partition.getPartitionId());
-            	System.err.println("************************************************");
+                System.err.println("************************************************");
+                System.err.printf("ERROR: Key '%d'%n", i);
+                System.err.printf("ERROR:   Predicted partition id '%d'%n", predictedPartitionId);
+                System.err.printf("ERROR:   Actual partition id '%d'%n", partition.getPartitionId());
+                System.err.println("************************************************");
             }
         }
-        
+
         anyInstance.getCluster().shutdown();
     }
-    
+
     /* A callable that returns the name of the node it is invoked on. We invoke
      * on the node that has a specific key, to find out which node that is.
      */
@@ -99,7 +99,7 @@ public class Application {
     static class MyCallable implements Callable<String>, HazelcastInstanceAware, Serializable {
 
         private transient HazelcastInstance hazelcastInstance;
-        
+
         @Override
         public String call() throws Exception {
             return this.hazelcastInstance.getName();
@@ -111,19 +111,19 @@ public class Application {
         }
     }
 
-    /* Calculate the hashcode and partition for a key 
+    /* Calculate the hashcode and partition for a key
      */
     static int[] predictedPartitionId(int key, int partitionCount) {
-    	
-    	int hashCode = Math.abs(murmurHash3Int(key));
-    	
-    	int partitionId = hashCode % partitionCount;
-    	
-    	int[] result = new int[2];
-    	result[0] = partitionId;
-    	result[1] = hashCode;
-    	
-    	return result;
+
+        int hashCode = Math.abs(murmurHash3Int(key));
+
+        int partitionId = hashCode % partitionCount;
+
+        int[] result = new int[2];
+        result[0] = partitionId;
+        result[1] = hashCode;
+
+        return result;
     }
 
     /* Murmur Hash 3, for integer input only. Java integers are 4 bytes.
@@ -131,17 +131,17 @@ public class Application {
      * See https://en.wikipedia.org/wiki/MurmurHash and
      * https://github.com/hazelcast/hazelcast/blob/master/hazelcast/src/main/java/com/hazelcast/internal/util/HashUtil.java
      */
-    public static int murmurHash3Int(int i) {    	
-    	// Hazelcast seed
+    public static int murmurHash3Int(int i) {
+        // Hazelcast seed
         int h = 0x01000193;
 
         // Hazelcast constants
         int c1 = 0xcc9e2d51;
         int c2 = 0x1b873593;
-    	
-    	// Byte order is Little Endian
+
+        // Byte order is Little Endian
         int k = Integer.reverseBytes(i);
-    	
+
         // Rotate 1
         k *= c1;
         k = (k << 15) | (k >>> 17);
@@ -154,7 +154,7 @@ public class Application {
 
         // Finalize, uses 4 byte size and two Murmur3 constants
         h ^= 4;
-    	h ^= h >>> 16;
+        h ^= h >>> 16;
         h *= 0x85ebca6b;
         h ^= h >>> 13;
         h *= 0xc2b2ae35;
