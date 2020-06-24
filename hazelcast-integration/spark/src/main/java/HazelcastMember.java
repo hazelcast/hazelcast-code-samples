20ba279757764cdbb1ddc843027c81202707ccad
Rafał Leszko <rafal@hazelcast.com>
2020-06-24 12:56:37 +0200

    Remove Old Hazelcast Integration Code Samples (#439)

--- hazelcast-integration/spark/src/main/java/HazelcastMember.java
+++
@@ -1,9 +0,0 @@
-import com.hazelcast.console.ConsoleApp;
-
-public class HazelcastMember {
-
-    public static void main(String[] args) throws Exception {
-        System.setProperty("hazelcast.local.localAddress", "127.0.0.1");
-        ConsoleApp.main(args);
-    }
-}
