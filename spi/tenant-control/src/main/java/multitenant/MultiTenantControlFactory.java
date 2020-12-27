a4d56d93a31c8e42e2e6123b48843471072aa863
lprimak <lenny@flowlogix.com>
2020-12-27 11:31:12 -0600

    fixed tenant control samples

--- spi/tenant-control/src/main/java/multitenant/MultiTenantControlFactory.java
+++ spi/tenant-control/src/main/java/multitenant/MultiTenantControlFactory.java
@@ -1,14 +1,17 @@
 package multitenant;
 
-import com.hazelcast.spi.tenantcontrol.DestroyEventContext;
 import com.hazelcast.spi.tenantcontrol.TenantControl;
 import com.hazelcast.spi.tenantcontrol.TenantControlFactory;
 
 public class MultiTenantControlFactory implements TenantControlFactory {
 
     @Override
-    public TenantControl saveCurrentTenant(DestroyEventContext event) {
-        String cacheName = event.getDistributedObjectName();
-        return new MultiTenantControl(cacheName);
+    public TenantControl saveCurrentTenant() {
+        return new MultiTenantControl();
+    }
+
+    @Override
+    public boolean isClassesAlwaysAvailable() {
+        return false;
     }
 }
