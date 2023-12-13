047358d032d8435b1b749e5bb26d5c69978998b0
Tomasz Gawęda <tomasz.gaweda@outlook.com>
2023-12-13 15:12:22 +0100

    Some additional fixes

--- hazelcast-integration/spring-hibernate-2ndlevel-cache/src/test/java/com/hazelcast/hibernate/springhibernate2lc/SpringHibernate2lcApplicationTest.java
+++ hazelcast-integration/spring-hibernate-2ndlevel-cache/src/test/java/com/hazelcast/hibernate/springhibernate2lc/SpringHibernate2lcApplicationTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringHibernate2lcApplicationTests {
+class SpringHibernate2lcApplicationTest {
 
 	@Test
 	void contextLoads() {
