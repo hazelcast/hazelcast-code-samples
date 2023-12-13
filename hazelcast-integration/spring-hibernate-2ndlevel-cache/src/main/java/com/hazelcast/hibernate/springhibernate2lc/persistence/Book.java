2a653afa4a363441f7fbca2b0fd96d9a671e8df7
Tomasz Gawęda <tomasz.gaweda@outlook.com>
2023-12-13 14:58:33 +0100

    Make Samples compile with JDK 17 Javax.persistence -> jakarta.persistence Coherent versions

--- hazelcast-integration/spring-hibernate-2ndlevel-cache/src/main/java/com/hazelcast/hibernate/springhibernate2lc/persistence/Book.java
+++ hazelcast-integration/spring-hibernate-2ndlevel-cache/src/main/java/com/hazelcast/hibernate/springhibernate2lc/persistence/Book.java
@@ -3,12 +3,12 @@ package com.hazelcast.hibernate.springhibernate2lc.persistence;
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
 
-import javax.persistence.Cacheable;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import jakarta.persistence.Cacheable;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 @Cacheable
