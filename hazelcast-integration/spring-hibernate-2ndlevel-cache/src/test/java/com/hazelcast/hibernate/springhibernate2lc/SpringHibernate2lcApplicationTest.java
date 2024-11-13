c2e935d90d8724531bd99648e88a9fdbcfd91ca0
Nishaat Rajabali <12186256+nishaatr@users.noreply.github.com>
2024-11-13 17:16:41 +0000

    Update SpringHibernate2lcApplicationTest.java

--- hazelcast-integration/spring-hibernate-2ndlevel-cache/src/test/java/com/hazelcast/hibernate/springhibernate2lc/SpringHibernate2lcApplicationTest.java
+++ hazelcast-integration/spring-hibernate-2ndlevel-cache/src/test/java/com/hazelcast/hibernate/springhibernate2lc/SpringHibernate2lcApplicationTest.java
@@ -9,6 +9,7 @@ import org.hibernate.Session;
 import org.hibernate.Version;
 import org.hibernate.stat.Statistics;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Disabled;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +22,7 @@ import java.util.Collection;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@Disabled("See https://hazelcast.atlassian.net/browse/DI-338")
 class SpringHibernate2lcApplicationTest {
 
 	private static Logger logger = LoggerFactory.getLogger(SpringHibernate2lcApplicationTest.class);
