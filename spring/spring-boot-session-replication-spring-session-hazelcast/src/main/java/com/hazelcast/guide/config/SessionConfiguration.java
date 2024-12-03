13a7526c00ee7203be998f415eee6dd34522e7bf
Frantisek Hartman <frant.hartm@gmail.com>
2024-12-03 10:53:30 +0100

    Move session replication with Spring Session Hazelcast back [DEX-300]

---
+++ spring/spring-boot-session-replication-spring-session-hazelcast/src/main/java/com/hazelcast/guide/config/SessionConfiguration.java
@@ -0,0 +1,77 @@
+package com.hazelcast.guide.config;
+
+import com.hazelcast.config.AttributeConfig;
+import com.hazelcast.config.Config;
+import com.hazelcast.config.IndexConfig;
+import com.hazelcast.config.IndexType;
+import com.hazelcast.config.SerializerConfig;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.session.FlushMode;
+import org.springframework.session.MapSession;
+import org.springframework.session.SaveMode;
+import org.springframework.session.config.SessionRepositoryCustomizer;
+import org.springframework.session.hazelcast.HazelcastIndexedSessionRepository;
+import org.springframework.session.hazelcast.HazelcastSessionSerializer;
+import org.springframework.session.hazelcast.PrincipalNameExtractor;
+import org.springframework.session.hazelcast.config.annotation.SpringSessionHazelcastInstance;
+import org.springframework.session.hazelcast.config.annotation.web.http.EnableHazelcastHttpSession;
+
+@Configuration
+@EnableHazelcastHttpSession
+class SessionConfiguration {
+
+    private final String SESSIONS_MAP_NAME = "spring-session-map-name";
+
+    @Bean
+    public SessionRepositoryCustomizer<HazelcastIndexedSessionRepository> customize() {
+        return (repository) -> {
+            repository.setFlushMode(FlushMode.IMMEDIATE);
+            repository.setSaveMode(SaveMode.ALWAYS);
+            repository.setSessionMapName(SESSIONS_MAP_NAME);
+            repository.setDefaultMaxInactiveInterval(900);
+        };
+    }
+
+    @Bean
+    @SpringSessionHazelcastInstance
+    public HazelcastInstance hazelcastInstance() {
+        Config config = new Config();
+        config.setClusterName("spring-session-cluster");
+
+        // Add this attribute to be able to query sessions by their PRINCIPAL_NAME_ATTRIBUTE's
+        AttributeConfig attributeConfig = new AttributeConfig()
+                .setName(HazelcastIndexedSessionRepository.PRINCIPAL_NAME_ATTRIBUTE)
+                .setExtractorClassName(PrincipalNameExtractor.class.getName());
+
+        // Configure the sessions map
+        config.getMapConfig(SESSIONS_MAP_NAME)
+                .addAttributeConfig(attributeConfig).addIndexConfig(
+                new IndexConfig(IndexType.HASH, HazelcastIndexedSessionRepository.PRINCIPAL_NAME_ATTRIBUTE));
+
+        // Use custom serializer to de/serialize sessions faster. This is optional.
+        // Note that, all members in a cluster and connected clients need to use the
+        // same serializer for sessions. For instance, clients cannot use this serializer
+        // where members are not configured to do so.
+        SerializerConfig serializerConfig = new SerializerConfig();
+        serializerConfig.setImplementation(new HazelcastSessionSerializer()).setTypeClass(MapSession.class);
+        config.getSerializationConfig().addSerializerConfig(serializerConfig);
+
+        return Hazelcast.newHazelcastInstance(config);
+    }
+
+    /* Hazelcast Client Instance Bean
+    @Bean
+    @SpringSessionHazelcastInstance
+    public HazelcastInstance hazelcastInstance() {
+        ClientConfig clientConfig = new ClientConfig();
+        clientConfig.getNetworkConfig().addAddress("127.0.0.1:5701");
+        clientConfig.getUserCodeDeploymentConfig().setEnabled(true).addClass(Session.class)
+                .addClass(MapSession.class).addClass(SessionUpdateEntryProcessor.class);
+        return HazelcastClient.newHazelcastClient(clientConfig);
+    }
+    */
+
+}
---
+++ spring/spring-boot-session-replication-spring-session-hazelcast/src/main/java/com/hazelcast/guide/controller/SessionController.java
@@ -0,0 +1,103 @@
+package com.hazelcast.guide.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.http.MediaType;
+import org.springframework.session.Session;
+import org.springframework.session.hazelcast.HazelcastIndexedSessionRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@RestController
+public class SessionController {
+
+    private static final String principalIndexName = HazelcastIndexedSessionRepository.PRINCIPAL_NAME_INDEX_NAME;
+    private static final DateFormat formatter = new SimpleDateFormat("HH:mm:ss");
+
+    final HazelcastIndexedSessionRepository sessionRepository;
+
+    public SessionController(HazelcastIndexedSessionRepository sessionRepository) {
+        this.sessionRepository = sessionRepository;
+    }
+
+    /**
+     * Creates a session for the request if there is no session of the request.
+     *
+     * @param principal Principal value of the session to be created
+     * @return Message indicating the session creation or abortion result.
+     *
+     */
+    @GetMapping("/create")
+    public String createSession(@RequestParam("principal") String principal, HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if (session == null) {
+            session = request.getSession();
+            session.setAttribute(principalIndexName, principal);
+            return "Session created: " + session.getId();
+        } else {
+            return "Session already exists: " + session.getId();
+        }
+    }
+
+    /**
+     * Lists all the sessions with the same {@link #principalIndexName} of the request's session.
+     *
+     * @return All sessions associated with this session's {@linzk #principalIndexName}.
+     */
+    @GetMapping(value = "/list", produces = MediaType.TEXT_HTML_VALUE)
+    public String listSessionsByPrincipal(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if (session == null) {
+            return "<html>No session found.</html>";
+        }
+        String principal = (String) session.getAttribute(principalIndexName);
+        Map<String, ? extends Session> sessions = sessionRepository.findByPrincipalName(principal);
+        return toHtmlTable(sessions.entrySet().stream().collect(Collectors.toMap(
+                e -> e.getKey().substring(0, 8),
+                e -> "Principal: " + session.getAttribute(principalIndexName))
+        ));
+    }
+
+    /**
+     * Returns the current session's details if the request has a session.
+     *
+     * @return Session details
+     */
+    @GetMapping(value = "/info", produces = MediaType.TEXT_HTML_VALUE)
+    public String getSessionInfo(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if (session == null) {
+            return "<html>No session found.</html>";
+        }
+        Map<String, Object> attributes = new LinkedHashMap();
+        attributes.put("sessionId", session.getId());
+        attributes.put("principal", session.getAttribute(principalIndexName));
+        attributes.put("created", formatter.format(new Date(session.getCreationTime())));
+        attributes.put("last accessed", formatter.format(new Date(session.getLastAccessedTime())));
+        return toHtmlTable(attributes);
+    }
+
+    private String toHtmlTable(Map<String, Object> attributes) {
+        StringBuilder html = new StringBuilder("<html>");
+        html.append("<table border=\"1\" cellpadding=\"5\" cellspacing=\"5\">");
+        attributes.forEach((k, v) -> addHtmlTableRow(html, k, v));
+        html.append("</table></html>");
+        return html.toString();
+    }
+
+    private void addHtmlTableRow(StringBuilder content, String key, Object value) {
+        content.append("<tr>")
+                    .append("<th>").append(key).append("</th>")
+                    .append("<td>").append(value).append("</td>")
+                .append("</tr>");
+    }
+
+}
