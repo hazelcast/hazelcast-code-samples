1aff7f9e77f219b3309c2682321603a68688cc72
Oliver Howell <oliver.howell@hazelcast.com>
2025-02-21 10:55:43 +0000

    DOC-391: move operator-jet-job-snapshot samples into code samples

---
+++ jet/operator-jet-job-snapshot/jet-pipelines/src/main/java/org/examples/jet/snapshot/JobV1.java
@@ -0,0 +1,32 @@
+package org.examples.jet.snapshot;
+
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.jet.pipeline.JournalInitialPosition;
+import com.hazelcast.jet.pipeline.Pipeline;
+import com.hazelcast.jet.pipeline.Sinks;
+import com.hazelcast.jet.pipeline.Sources;
+
+public class JobV1 {
+
+    public static void main(String[] args) {
+        var p = Pipeline.create();
+        var transactionSource = Sources.mapJournal("transaction", JournalInitialPosition.START_FROM_OLDEST);
+        var loggerSink = Sinks.logger();
+        p.readFrom(transactionSource)
+                .withIngestionTimestamps()
+                .setName("Emit Transactions")
+                .map(e -> {
+                    System.out.printf("The transaction '%s' is being executed in 'job-v1'\n", e.getKey());
+                    // execute the transaction
+                    return String.format("[Job V1] transaction:'%s' payload:'%s'", e.getKey(), e.getValue());
+                })
+                .setName("Apply Transactions")
+                .writeTo(loggerSink)
+                .setName("Log Transactions");
+
+        HazelcastInstance hz = Hazelcast.bootstrappedInstance();
+        hz.getJet().newJob(p);
+    }
+
+}
