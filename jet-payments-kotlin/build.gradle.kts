hazelcast/hazelcast-code-samples

commit f0a4c87e27fdbf763aa0c4366a9f8dd9fa423a02

DOC-500 add Jet payments demo (#720)
Import https://github.com/knurl/jetpayments to official repo.

https://hazelcast.atlassian.net/browse/DOC-500

Checklist:
- [x] Request reviewers if possible
- [x] README.md or README.adoc is created with a description _what_
example does and _how_ to run it
- [ ] Add tests that reproduce the associated tutorial or similar
scenario

jet-payments-kotlin/build.gradle.kts
@@ -1,0 +1,83 @@
+plugins {
+    kotlin("jvm") version "2.1.21"
+    kotlin("plugin.serialization") version "2.1.21"
+    application
+}
+
+group = "org.hazelcast"
+version = "1.0"
+
+private val javaVersion = 21
+private val kotlinVersion = "2.1.21" // can't substitute in plugins section above
+private val hazelcastVersion = "5.5.0"
+
+repositories {
+    mavenCentral()
+    maven {
+        url = uri("https://packages.confluent.io/maven/")
+    }
+    maven {
+        url = uri("https://repository.hazelcast.com/release/")
+    }
+    mavenLocal()
+    gradlePluginPortal()
+}
+
+dependencies {
+    testImplementation("io.kotest:kotest-runner-junit5:5.8.0")
+    testImplementation("io.kotest:kotest-assertions-core:5.8.0")
+    testImplementation("io.kotest:kotest-property:5.8.0")
+    // For testing coroutines
+    testImplementation("io.kotest:kotest-assertions-core-jvm:5.8.0")
+
+    /*
+     * Core kotlin libs. We'll need these on all Hazelcast members when using Jet.
+     */
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.0")
+
+    /*
+     * Hazelcast.
+     */
+    implementation("com.hazelcast:hazelcast:$hazelcastVersion")
+    implementation("com.hazelcast.jet:hazelcast-jet-kafka:$hazelcastVersion")
+
+    /*
+     * Kafka
+     */
+    implementation("com.google.cloud.hosted.kafka:managed-kafka-auth-login-handler:1.0.6")
+    implementation("org.slf4j:slf4j-nop:2.0.16") // Quiets an annoying message SLF4J: Failed to load class org.slf4j.impl.StaticLoggerBinder
+}
+
+application {
+    mainClass.set("org.hazelcast.jetpayments.MainKt")
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    compilerOptions {
+        allWarningsAsErrors = true
+        progressiveMode = true
+        freeCompilerArgs.addAll(listOf(
+            "-Xjsr305=strict",
+        ))
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(javaVersion)
+        vendor = JvmVendorSpec.ORACLE
+    }
+}
+
+kotlin {
+    jvmToolchain {
+        languageVersion = JavaLanguageVersion.of(javaVersion)
+        vendor = JvmVendorSpec.ORACLE
+    }
+}

jet-payments-kotlin/src/test/kotlin/PaymentsFlowDurationTest.kt
@@ -1,0 +1,215 @@
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.consumeAsFlow
+import kotlinx.coroutines.flow.zip
+import org.hazelcast.jetpayments.*
+import kotlin.math.absoluteValue
+import kotlin.test.Test
+import kotlin.time.Duration.Companion.microseconds
+import kotlin.time.Duration.Companion.seconds
+
+class PaymentsTest {
+    private val logger = ElapsedTimeLogger("PaymentsTest")
+    private val context = Dispatchers.Default + SupervisorJob()
+
+    @Test
+    fun `test runPaymentsFlow with different test periods`() =
+        runBlocking(context) {
+            val numTests = 4
+            val testRuns = List(numTests) { 1 }
+            //val testRuns = generateSequence(1) { it + 1 }.take(numTests).toList()
+            val numPaymentsArray = testRuns.map { numFailureCycles ->
+                "$numFailureCycles failure cycles"
+            }
+
+            fun showNthTest(testNum: Int) = TextBox(buildList {
+                numPaymentsArray.forEachIndexed { index, s ->
+                    val testStr = "TEST $index: $s"
+                    val line = if (index == testNum) "run-> $testStr <-now"
+                    else testStr
+                    add(line)
+                }
+            }).center().addBorder().log(logger)
+
+            PaymentsRun().use { paymentsRun ->
+                testRuns.forEachIndexed { testNum, numFailureCycles ->
+                    Epoch.reset()
+                    showNthTest(testNum)
+                    paymentsRun.run(numFailureCycles)
+                }
+            }
+        }
+
+    @Test
+    fun `test KafkaCluster to ensure re-readability`() = runBlocking(context) {
+        val numPayments = 2000
+        val paymentGenerator = PaymentGenerator(seededRandom)
+        val consumerGroup = AppConfig.kafkaVerifyPaymentsCG.uniqify()
+        val channel = Channel<PaymentRequest>(numPayments)
+        val channelFlow = channel.consumeAsFlow()
+
+        KafkaCluster<Int, String>(
+            AppConfig.kafkaTopicName.uniqify(), 0
+        ).use { kafka ->
+            withContext(Dispatchers.IO) {
+                val kafkaFlow = kafka.consumeAsFlow(
+                    consumerGroup, numPayments
+                ) { it.toPaymentRequest() }
+
+                coroutineScope {
+                    launch {
+                        var numChecked = 0
+                        channelFlow.zip(kafkaFlow) { checkPmtReq, pmtReq ->
+                            pmtReq to checkPmtReq
+                        }.collect { (pmtReq, checkPmtReq) ->
+                            check(pmtReq == checkPmtReq) { "Payment mismatch $pmtReq != $checkPmtReq" }
+                            numChecked++
+                            if (numChecked % 100 == 0) logger.log("Checked $numChecked/$numPayments")
+                        }
+                    }
+
+                    launch {
+                        var numPublished = 0
+                        paymentGenerator.newPaymentRequestSeq().take(numPayments)
+                            .forEach { pmtreq ->
+                                kafka.publish(pmtreq.toJsonString())
+                                channel.send(pmtreq)
+                                numPublished++
+                                if (numPublished % 100 == 0) logger.log("Published $numPublished/$numPayments")
+                            }
+                        channel.close() // Close send channel here so receiver will terminate
+                    }
+                }
+            }
+        }
+
+        logger.log("Validated all payments")
+    }
+
+    @Test
+    fun `test creation of Canvas Timescales`() = runBlocking(context) {
+        val canvasSizeSeq = generateSequence(54) { it + 6 }.takeWhile { it <= 196 }
+        val merchantMap =
+            MerchantGenerator(AppConfig.numMerchants, seededRandom).merchantMap
+        canvasSizeSeq.forEach { canvasSize ->
+            val minTime = 0.seconds
+            val totalTimeSeq =
+                generateSequence(canvasSize.seconds) { it + canvasSize.seconds / 4 }.takeWhile { it <= canvasSize.seconds * 2 }
+            totalTimeSeq.forEach { totalTime ->
+                val maxTime = minTime + totalTime
+
+                val timespans = merchantMap.values.map { merchant ->
+                    val numRanges = canvasSize / 16
+                    (1..numRanges * 2).map {
+                        seededRandom.nextLong(
+                            minTime.inWholeMicroseconds, maxTime.inWholeMicroseconds
+                        ).microseconds
+                    }.sorted().chunked(2).map { (start, endInclusive) ->
+                        TimeRange(
+                            merchant.name.first().toString(), start, endInclusive
+                        )
+                    }.let { timeRanges ->
+                        TimeSpan(merchant.name, timeRanges)
+                    }
+                }.sorted()
+
+                val canvas = Canvas(timespans, canvasSize)
+                val lines = canvas.draw()
+                assert(lines.map { it.length }.all { it == lines.first().length })
+                TextBox(buildList {
+                    add("Canvas size: ${canvas.canvasSize}")
+                    add("totalCanvasTime: ${canvas.totalCanvasTime}")
+                }).justify().addBorder().log(logger)
+                lines.log(logger)
+            }
+        }
+    }
+
+    @Test
+    fun `test merchant distribution verification logic`() = runBlocking(context) {
+        val numPayments = 100
+        val clusterSize = AppConfig.clusterSize
+        val runLength = numPayments / clusterSize / 2
+
+        val rotationSeq = sequence {
+            while (true) {
+                repeat(clusterSize) { i ->
+                    repeat(runLength) { yield(i) }
+                }
+            }
+        }
+
+        val client = HzCluster("dev", AppConfig.clusterSize).getClient()
+        val paymentReceiptMap =
+            client.getMap<Int, PaymentReceipt>(AppConfig.paymentReceiptMapName)
+        val paymentMemberDistributionCheckMap =
+            client.getMap<String, List<TimeRange>>(AppConfig.paymentOnOneNodeCheckMapName)
+        val channel = Channel<PaymentRequest>(numPayments)
+
+        logger.log("Populating receipt map")
+        (PaymentGenerator(seededRandom).newPaymentRequestSeq() zip rotationSeq).take(
+            numPayments
+        ).forEach { (payreq, rotate) ->
+            val paidOnMember =
+                (payreq.merchantId.hashCode() + rotate).absoluteValue % clusterSize
+            val receipt = PaymentReceipt(payreq, true, paidOnMember)
+            paymentReceiptMap.put(receipt.paymentId, receipt)
+            channel.send(payreq)
+        }
+        channel.close() // Close send channel here receiver will terminate
+
+        /*
+         * Now run our Jet job to process down the payment receipts.
+         */
+        logger.log("Running PaymentMemberCheckPipeline")
+        PaymentMemberCheckPipeline(client).use { it.run() }
+
+        val timeRangeIteratorByMerchant =
+            paymentMemberDistributionCheckMap.mapValues { (_, timeRanges) ->
+                timeRanges.iterator()
+            }
+
+        logger.log("Checking that all timeRanges match")
+        var numChecked = 0
+        for (payreq in channel) {
+            if (numChecked % 10 == 0) logger.log("Checked #$numChecked")
+            val timeRange = timeRangeIteratorByMerchant[payreq.merchantId]!!.next()
+            val receipt = paymentReceiptMap[payreq.paymentId]!!
+            assert(fontEtched(receipt.onMember) == timeRange.marker) { "paidOnMember mismatch ${receipt.onMember} != ${timeRange.marker} for $payreq" }
+            assert(timeRange.start == timeRange.endInclusive) { "start/endInclusive mismatch $timeRange for $payreq" }
+            assert(timeRange.start == receipt.timePaid) { "timePaid mismatch $timeRange for $receipt" }
+            numChecked++
+        }
+
+        logger.log("Validated all payments")
+    }
+
+    @Test
+    fun `test TallyField generate function`() = runBlocking(context) {
+        val merchant = MerchantGenerator(
+            1, seededRandom
+        ).merchantMap.values.first().shortName
+        val tallySeq = generateSequence(0) { i -> i + 1 }.map { i ->
+            ReceiptWatcher.NodeTally(11 + i * 10, i)
+        }
+        val metricsSummary = "[9999 QUD ⇒ 9999 WKG ⇒ 9999 FIN]➣"
+        fun generateLogLine(fields: Iterable<String>): String =
+            "$metricsSummary⟦ ${fields.joinToString(" | ")} ⟧"
+
+        val numFields = AppConfig.numMerchants
+        val prefixLen = 20
+        val emptyFieldLine = generateLogLine(List(numFields) { "" })
+        val fieldWidth =
+            (AppConfig.screenWidth - emptyFieldLine.numCodepoints() - prefixLen) / numFields
+        TextBox(
+            listOf("width=$fieldWidth"), borderStyle = TextBox.BorderStyle.DOUBLE
+        ).log(logger)
+        for (numNodes in 1..9) {
+            val tally = tallySeq.take(numNodes).toList()
+            val fields = List(numFields) {
+                ReceiptWatcher.TallyField(fieldWidth, merchant, tally).generate()
+            }
+            logger.log(generateLogLine(fields))
+        }
+    }
+}

jet-payments-kotlin/.gitignore
@@ -1,0 +1,4 @@
+.gradle/
+.idea/
+.kotlin/
+build/

jet-payments-kotlin/LICENSE
@@ -1,0 +1,201 @@
+                                 Apache License
+                           Version 2.0, January 2004
+                        http://www.apache.org/licenses/
+
+   TERMS AND CONDITIONS FOR USE, REPRODUCTION, AND DISTRIBUTION
+
+   1. Definitions.
+
+      "License" shall mean the terms and conditions for use, reproduction,
+      and distribution as defined by Sections 1 through 9 of this document.
+
+      "Licensor" shall mean the copyright owner or entity authorized by
+      the copyright owner that is granting the License.
+
+      "Legal Entity" shall mean the union of the acting entity and all
+      other entities that control, are controlled by, or are under common
+      control with that entity. For the purposes of this definition,
+      "control" means (i) the power, direct or indirect, to cause the
+      direction or management of such entity, whether by contract or
+      otherwise, or (ii) ownership of fifty percent (50%) or more of the
+      outstanding shares, or (iii) beneficial ownership of such entity.
+
+      "You" (or "Your") shall mean an individual or Legal Entity
+      exercising permissions granted by this License.
+
+      "Source" form shall mean the preferred form for making modifications,
+      including but not limited to software source code, documentation
+      source, and configuration files.
+
+      "Object" form shall mean any form resulting from mechanical
+      transformation or translation of a Source form, including but
+      not limited to compiled object code, generated documentation,
+      and conversions to other media types.
+
+      "Work" shall mean the work of authorship, whether in Source or
+      Object form, made available under the License, as indicated by a
+      copyright notice that is included in or attached to the work
+      (an example is provided in the Appendix below).
+
+      "Derivative Works" shall mean any work, whether in Source or Object
+      form, that is based on (or derived from) the Work and for which the
+      editorial revisions, annotations, elaborations, or other modifications
+      represent, as a whole, an original work of authorship. For the purposes
+      of this License, Derivative Works shall not include works that remain
+      separable from, or merely link (or bind by name) to the interfaces of,
+      the Work and Derivative Works thereof.
+
+      "Contribution" shall mean any work of authorship, including
+      the original version of the Work and any modifications or additions
+      to that Work or Derivative Works thereof, that is intentionally
+      submitted to Licensor for inclusion in the Work by the copyright owner
+      or by an individual or Legal Entity authorized to submit on behalf of
+      the copyright owner. For the purposes of this definition, "submitted"
+      means any form of electronic, verbal, or written communication sent
+      to the Licensor or its representatives, including but not limited to
+      communication on electronic mailing lists, source code control systems,
+      and issue tracking systems that are managed by, or on behalf of, the
+      Licensor for the purpose of discussing and improving the Work, but
+      excluding communication that is conspicuously marked or otherwise
+      designated in writing by the copyright owner as "Not a Contribution."
+
+      "Contributor" shall mean Licensor and any individual or Legal Entity
+      on behalf of whom a Contribution has been received by Licensor and
+      subsequently incorporated within the Work.
+
+   2. Grant of Copyright License. Subject to the terms and conditions of
+      this License, each Contributor hereby grants to You a perpetual,
+      worldwide, non-exclusive, no-charge, royalty-free, irrevocable
+      copyright license to reproduce, prepare Derivative Works of,
+      publicly display, publicly perform, sublicense, and distribute the
+      Work and such Derivative Works in Source or Object form.
+
+   3. Grant of Patent License. Subject to the terms and conditions of
+      this License, each Contributor hereby grants to You a perpetual,
+      worldwide, non-exclusive, no-charge, royalty-free, irrevocable
+      (except as stated in this section) patent license to make, have made,
+      use, offer to sell, sell, import, and otherwise transfer the Work,
+      where such license applies only to those patent claims licensable
+      by such Contributor that are necessarily infringed by their
+      Contribution(s) alone or by combination of their Contribution(s)
+      with the Work to which such Contribution(s) was submitted. If You
+      institute patent litigation against any entity (including a
+      cross-claim or counterclaim in a lawsuit) alleging that the Work
+      or a Contribution incorporated within the Work constitutes direct
+      or contributory patent infringement, then any patent licenses
+      granted to You under this License for that Work shall terminate
+      as of the date such litigation is filed.
+
+   4. Redistribution. You may reproduce and distribute copies of the
+      Work or Derivative Works thereof in any medium, with or without
+      modifications, and in Source or Object form, provided that You
+      meet the following conditions:
+
+      (a) You must give any other recipients of the Work or
+          Derivative Works a copy of this License; and
+
+      (b) You must cause any modified files to carry prominent notices
+          stating that You changed the files; and
+
+      (c) You must retain, in the Source form of any Derivative Works
+          that You distribute, all copyright, patent, trademark, and
+          attribution notices from the Source form of the Work,
+          excluding those notices that do not pertain to any part of
+          the Derivative Works; and
+
+      (d) If the Work includes a "NOTICE" text file as part of its
+          distribution, then any Derivative Works that You distribute must
+          include a readable copy of the attribution notices contained
+          within such NOTICE file, excluding those notices that do not
+          pertain to any part of the Derivative Works, in at least one
+          of the following places: within a NOTICE text file distributed
+          as part of the Derivative Works; within the Source form or
+          documentation, if provided along with the Derivative Works; or,
+          within a display generated by the Derivative Works, if and
+          wherever such third-party notices normally appear. The contents
+          of the NOTICE file are for informational purposes only and
+          do not modify the License. You may add Your own attribution
+          notices within Derivative Works that You distribute, alongside
+          or as an addendum to the NOTICE text from the Work, provided
+          that such additional attribution notices cannot be construed
+          as modifying the License.
+
+      You may add Your own copyright statement to Your modifications and
+      may provide additional or different license terms and conditions
+      for use, reproduction, or distribution of Your modifications, or
+      for any such Derivative Works as a whole, provided Your use,
+      reproduction, and distribution of the Work otherwise complies with
+      the conditions stated in this License.
+
+   5. Submission of Contributions. Unless You explicitly state otherwise,
+      any Contribution intentionally submitted for inclusion in the Work
+      by You to the Licensor shall be under the terms and conditions of
+      this License, without any additional terms or conditions.
+      Notwithstanding the above, nothing herein shall supersede or modify
+      the terms of any separate license agreement you may have executed
+      with Licensor regarding such Contributions.
+
+   6. Trademarks. This License does not grant permission to use the trade
+      names, trademarks, service marks, or product names of the Licensor,
+      except as required for reasonable and customary use in describing the
+      origin of the Work and reproducing the content of the NOTICE file.
+
+   7. Disclaimer of Warranty. Unless required by applicable law or
+      agreed to in writing, Licensor provides the Work (and each
+      Contributor provides its Contributions) on an "AS IS" BASIS,
+      WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+      implied, including, without limitation, any warranties or conditions
+      of TITLE, NON-INFRINGEMENT, MERCHANTABILITY, or FITNESS FOR A
+      PARTICULAR PURPOSE. You are solely responsible for determining the
+      appropriateness of using or redistributing the Work and assume any
+      risks associated with Your exercise of permissions under this License.
+
+   8. Limitation of Liability. In no event and under no legal theory,
+      whether in tort (including negligence), contract, or otherwise,
+      unless required by applicable law (such as deliberate and grossly
+      negligent acts) or agreed to in writing, shall any Contributor be
+      liable to You for damages, including any direct, indirect, special,
+      incidental, or consequential damages of any character arising as a
+      result of this License or out of the use or inability to use the
+      Work (including but not limited to damages for loss of goodwill,
+      work stoppage, computer failure or malfunction, or any and all
+      other commercial damages or losses), even if such Contributor
+      has been advised of the possibility of such damages.
+
+   9. Accepting Warranty or Additional Liability. While redistributing
+      the Work or Derivative Works thereof, You may choose to offer,
+      and charge a fee for, acceptance of support, warranty, indemnity,
+      or other liability obligations and/or rights consistent with this
+      License. However, in accepting such obligations, You may act only
+      on Your own behalf and on Your sole responsibility, not on behalf
+      of any other Contributor, and only if You agree to indemnify,
+      defend, and hold each Contributor harmless for any liability
+      incurred by, or claims asserted against, such Contributor by reason
+      of your accepting any such warranty or additional liability.
+
+   END OF TERMS AND CONDITIONS
+
+   APPENDIX: How to apply the Apache License to your work.
+
+      To apply the Apache License to your work, attach the following
+      boilerplate notice, with the fields enclosed by brackets "[]"
+      replaced with your own identifying information. (Don't include
+      the brackets!)  The text should be enclosed in the appropriate
+      comment syntax for the file format. We also recommend that a
+      file or class name and description of purpose be included on the
+      same "printed page" as the copyright notice for easier
+      identification within third-party archives.
+
+   Copyright [yyyy] [name of copyright owner]
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.

jet-payments-kotlin/gradle.properties
@@ -1,0 +1,2 @@
+kotlin.code.style=official
+org.gradle.jvmargs=-XX:+UseZGC

jet-payments-kotlin/gradle/gradle-daemon-jvm.properties
@@ -1,0 +1,2 @@
+#This file is generated by updateDaemonJvm
+toolchainVersion=21

jet-payments-kotlin/gradle/wrapper/gradle-wrapper.properties
@@ -1,0 +1,7 @@
+distributionBase=GRADLE_USER_HOME
+distributionPath=wrapper/dists
+distributionUrl=https\://services.gradle.org/distributions/gradle-8.10-bin.zip
+networkTimeout=10000
+validateDistributionUrl=true
+zipStoreBase=GRADLE_USER_HOME
+zipStorePath=wrapper/dists

commit 7f050db5c81b7390580479a28e210209db4f65e2

Fix _some_ `jet-payments-kotlin` compilation errors
Fix _some_ errors introduced with new project in https://github.com/hazelcast/hazelcast-code-samples/pull/720

jet-payments-kotlin/build.gradle.kts
@@ -27,6 +27,7 @@ dependencies {
     testImplementation("io.kotest:kotest-runner-junit5:5.8.0")
     testImplementation("io.kotest:kotest-assertions-core:5.8.0")
     testImplementation("io.kotest:kotest-property:5.8.0")
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
     // For testing coroutines
     testImplementation("io.kotest:kotest-assertions-core-jvm:5.8.0")
 
@@ -71,13 +72,11 @@ tasks.withType<Test> {
 java {
     toolchain {
         languageVersion = JavaLanguageVersion.of(javaVersion)
-        vendor = JvmVendorSpec.ORACLE
     }
 }
 
 kotlin {
     jvmToolchain {
         languageVersion = JavaLanguageVersion.of(javaVersion)
-        vendor = JvmVendorSpec.ORACLE
     }
 }

jet-payments-kotlin/src/test/kotlin/PaymentsFlowDurationTest.kt
@@ -8,8 +8,8 @@ import kotlin.test.Test
 import kotlin.time.Duration.Companion.microseconds
 import kotlin.time.Duration.Companion.seconds
 
-class PaymentsTest {
-    private val logger = ElapsedTimeLogger("PaymentsTest")
+class PaymentsFlowDurationTest {
+    private val logger = ElapsedTimeLogger("PaymentsFlowDurationTest")
     private val context = Dispatchers.Default + SupervisorJob()
 
     @Test