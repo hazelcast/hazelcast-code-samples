9bf5fc59f610301e4f6c8ca7900146d543e49c92
Fabrizio Cannizzo <fabrizio.cannizzo@hazelcast.com>
2025-09-22 17:45:33 +0100

    removed unused import

--- testing/src/test/java/com/hazelcast/samples/testing/junit5/OrderEnrichmentPipelineTest.java
+++ testing/src/test/java/com/hazelcast/samples/testing/junit5/OrderEnrichmentPipelineTest.java
@@ -22,8 +22,6 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.concurrent.CompletionException;
-
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
