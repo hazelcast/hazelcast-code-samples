971d97a51edf03609b43f7207352e39ec9fe1957
Fabrizio Cannizzo <fabrizio.cannizzo@hazelcast.com>
2025-09-25 14:34:24 +0100

    refactored to simplify test method

--- testing/src/test/java/com/hazelcast/samples/testing/samples/junit4/MyMapListenerTest.java
+++ testing/src/test/java/com/hazelcast/samples/testing/samples/junit4/MyMapListenerTest.java
@@ -6,6 +6,8 @@ import com.hazelcast.map.IMap;
 import com.hazelcast.map.listener.EntryUpdatedListener;
 import com.hazelcast.test.HazelcastTestSupport;
 import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
@@ -15,32 +17,35 @@ import static org.mockito.Mockito.timeout;
 import static org.mockito.Mockito.verify;
 
 @RunWith(JUnit4.class)
-public class MyMapListenerTest
-        extends HazelcastTestSupport {
+public class MyMapListenerTest extends HazelcastTestSupport {
+
+    private HazelcastInstance instance;
+
+    @BeforeEach
+    public void setUp() {
+        // create Hazelcast member
+        instance = createHazelcastInstance();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        instance.shutdown();
+    }
 
     @Test
     public void updateTriggersListener() {
-        // create Hazelcast member
-        HazelcastInstance instance = createHazelcastInstance();
+        // create mock listener
+        EntryUpdatedListener<String, String> mockListener = mock(EntryUpdatedListener.class);
 
-        try {
-            // create mock listener
-            EntryUpdatedListener<String, String> mockListener = mock(EntryUpdatedListener.class);
+        // register the listener
+        IMap<String, String> map = instance.getMap("test-map");
+        map.addEntryListener(mockListener, true);
 
-            // register the listener
-            IMap<String, String> map = instance.getMap("test-map");
-            map.addEntryListener(mockListener, true);
+        // insert and update an entry
+        map.put("key1", "initial");
+        map.put("key1", "updated");
 
-            // insert and update an entry
-            map.put("key1", "initial");
-            map.put("key1", "updated");
-
-            // verify the listener received the update
-            verify(mockListener, timeout(1000).times(1)).entryUpdated(any(EntryEvent.class));
-        } finally {
-            if (instance != null) {
-                instance.shutdown();
-            }
-        }
+        // verify the listener received the update
+        verify(mockListener, timeout(1000).times(1)).entryUpdated(any(EntryEvent.class));
     }
 }
