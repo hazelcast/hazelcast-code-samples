3735bd4460ca8854a8fce6618a2e2925a4b412e9
Tomasz Gawęda <tomasz.gaweda@outlook.com>
2026-02-11 16:00:32 +0100

    Added JavaDoc

--- spring/resilient-sessions/src/main/java/com/hazelcast/samples/model/Basket.java
+++ spring/resilient-sessions/src/main/java/com/hazelcast/samples/model/Basket.java
@@ -9,6 +9,11 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Main basket object representation
+ * @param principalName username of the user that's logged into the shop and is putting items in the basket.
+ * @param items a collection of items, added by the user
+ */
 public record Basket (String principalName, List<BasketItem> items) implements Serializable {
 
     public static class Serializer implements CompactSerializer<Basket> {
@@ -18,6 +23,7 @@ public record Basket (String principalName, List<BasketItem> items) implements S
         public Basket read(CompactReader compactReader) {
             String principalName1 = compactReader.readString("principalName");
             BasketItem[] items1 = compactReader.readArrayOfCompact("items", BasketItem.class);
+            assert items1 != null;
             return new Basket(principalName1, new ArrayList<>(List.of(items1)));
         }
 
